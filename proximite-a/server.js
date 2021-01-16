a0afc51a0c17ea833c94c0989c266f7fe3ea6dd3
Ioan SAVOIU <ioan.savoiu@etu.univ-nantes.fr>
2021-01-16 12:15:57 +0100

    Configuring Express app for static files

--- proximite-a/server.js
+++ proximite-a/server.js
@@ -4,17 +4,26 @@
 // Load usefull expressjs and nodejs objects / modules
 var express = require('express');
 var path = require('path');
-
+var port = 3003;
 var app = express();
 
 // Minimum routing: serve static content from the html directory
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '../__common-logos__')));
 
+
 // You can then add whatever routing code you need
 
+
 // This module is exported and served by the main server.js located
-// at the root of this set of projects. You can access it by lanching the main
+// at the root of this set of projects. You can access it by launching the main
 // server and visiting http(s)://127.0.0.1:8080/name_of_you_project/ (if on a local server)
 // or more generally: http(s)://server_name:port/name_of_you_project/
+app.listen(port, err=>{
+    if (err){
+        return console.log("ERROR", err)
+    }
+    console.log(`Listening on port ${port}`)
+})
+
 module.exports = app;
