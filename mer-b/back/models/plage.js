56a1074633395c67a8573902eb6c869e2b4ed970
tud <tudual.quinio@protonmail.com>
2021-01-21 16:58:22 +0100

    fetching beaches

--- mer-b/back/models/plage.js
+++ mer-b/back/models/plage.js
@@ -1,10 +1,11 @@
 'user strict';
+const fetch = require('node-fetch');
 
 /**
  * streetmap output :
  * - type : surface : ["sand", "sable", "gravel", "ground", "asphalt", "grass",
  *                     "pebblestone",  "shingle", "shingles", "fine_gravel",
- *                     "sable_et_gallet", "stone"]
+ *                     "sable_et_gallet", "stone", "s", "dirt/sand"]
  * - naturisme : - nudism : yes/permissive/-/customary/obligatory/designated
  *               - naturism : yes/-
  * - planning : - harbour : yes
@@ -23,7 +24,7 @@ exports.getbyfilter = function(req) {
         const [filtre, arg] = e.split('=');
 
         switch (filtre) {
-            case "latitude":  filtres.latitude = parseFloat(arg);
+            case "latitude": filtres.latitude = parseFloat(arg);
             break;
             case "longitude":  filtres.longitude = parseFloat(arg);
             break;
@@ -50,11 +51,30 @@ exports.getbyfilter = function(req) {
 
     let arc = 9000/(6371*Math.PI);
 
-    let latTop = arc + filtres.latitude;
-    let latBot = arc - filtres.latitude;
-    let longLeft = arc + filtres.longitude;
-    let longRight = arc - filtres.longitude;
-    console.log(latTop);
+    let latTop = filtres.latitude + arc;
+    let latBot = filtres.latitude - arc;
+    let longLeft = filtres.longitude + arc;
+    let longRight = filtres.longitude - arc;
+
+    const ask = `?data=%5Bout%3Ajson%5D%5Bbbox%3A${latBot}%2C${longLeft}%2C${latTop}%2C${longRight}%5D%3B%0D%0A%28%0D%0A++area%5Bname%3D%22France%22%5D%3B%0D%0A++node%28area%29%5B%22natural%22%3D%22beach%22%5D%3B%0D%0A%29%3B%0D%0Aout%3B&target=compact`;
+    const cst = require("../constants/openstreetmap")
+
+    async function getbeaches() {
+        let response = await fetch(cst.api_url1+ask);
+
+        if (!response.ok) {
+            const message = `An error has occured: ${response.status}`;
+            throw new Error(message);
+        }
+
+        let data = await response.json()
+        return data;
+    }
+    getbeaches().then(data => {
+        
+        console.log(data)
+
+    })
 
     // will be replaced by am API call
     plages = [
