5fd1ef30b53cc079ab0ac649b1da034bc9961405
Loïc BOURSIN <loic.boursin@etu.univ-nantes.fr>
2021-01-22 15:28:39 +0100

    update addressPage

--- parents-b/public/js/addressPage.js
+++ parents-b/public/js/addressPage.js
@@ -1,6 +1,11 @@
 let initAddress = function(){
     d3.select(".button-next-address").on("click", function (){
         mySlidr.slide("age-page");
+
         initAge();
     });
 };
+
+new AddressAutocomplete('#address1', function (result) {
+    console.log(result);
+});
