3602e02218f7cc80cfbd3466366edcd8ef1473cd
Nakline <nathan.genaudeau@gmail.com>
2021-01-22 18:40:00 +0100

    Ajout de la page de recherche de resultat

--- parents-b/public/js/searchingResultsPage.js
+++ parents-b/public/js/searchingResultsPage.js
@@ -1,6 +1,29 @@
 let initSearchingResults = function(){
-    d3.select(".button-next-searching-results").on("click", function (){
+    setTimeout(function(){
         mySlidr.slide("results-page");
         initResults();
+    }, 4000);
+
+    anime({
+        targets: '#oya-searching-results',
+        translateY: '-5%',
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        loop: true
+    });
+
+
+    anime({
+        targets: '#left-gear-eye',
+        rotate: 1080,
+        duration: 50000,
+        loop: true,
+    });
+
+    anime({
+        targets: '#right-gear-eye',
+        rotate: 1080,
+        duration: 50000,
+        loop: true,
     });
 };
