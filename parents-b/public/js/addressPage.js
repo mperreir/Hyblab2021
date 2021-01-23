63f5031179ef83c03fe85c7ed4cbf4a4de9bd70c
ThibautPerr <77663343+ThibautPerr@users.noreply.github.com>
2021-01-23 21:41:24 +0100

    Fix adress slide (#14)
    * Fix : slide sur la page adress qui ne fonctionnait pas
    
    Un change est passé sur la mr sans que ce soit voulu

--- parents-b/public/js/addressPage.js
+++ parents-b/public/js/addressPage.js
@@ -11,7 +11,7 @@ let initAddress = function(){
     });
 
     d3.select(".button-next-address-from-nevermind").on("click", function (){
-        slidrHorizontal.slide("age-page");
+        mySlidr.slide("age-page");
         initAge();
     });
 };
--- parents-b/public/js/index.js
+++ parents-b/public/js/index.js
@@ -4,20 +4,14 @@ let mySlidr = slidr.create('slidr',{
     controls: 'none',
     direction: 'vertical',
     fade: true,
-    keyboard: true,
-    overflow: true,
+    keyboard: false,
+    overflow: false,
     pause: false,
     theme: '#222',
     timing: { 'fade': '0.5s ease-in' },
-    touch: true,
+    touch: false,
     transition: 'fade'
   }).start();
-
-let slidrHorizontal = slidr.create('slidr', {
-    direction: 'horizontal',
-    theme: '#222',
-    timing: { 'horizontal': '0.5s ease-in' },
-}).start();
 
 // on s'occupe de la 1ère slide
 initHome();
