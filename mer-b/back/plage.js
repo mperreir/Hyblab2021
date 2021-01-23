b8be0c03720679d810058d340a2fee6f4d87fe97
tud <tudual.quinio@protonmail.com>
2021-01-23 14:25:48 +0100

    ouptut in good format

--- mer-b/back/plage.js
+++ mer-b/back/plage.js
@@ -185,52 +185,97 @@ exports.getbyfilter = async function(req) {
     }
     plages = filter(plages, 30);
 
-    if (filtres.hasOwnProperty("weather") || filtres.hasOwnProperty("time") || filtres.hasOwnProperty("sea")) {
 
-        for (const node of plages) {
+    // fetching the weather for each beach
+    weather = [];
 
-            const response_weather = await fetch(cst.openweather.api_url + `lat=${node.latitude}&lon=${node.longitude}&appid=${cst.openweather.key}`);
+    for (const node of plages) {
 
-            if (!response_weather.ok) {
-                if (response_weather.status == 401) {
-                    return `Error: You need to input an an API key in the file: mer-b/back/constants.json`;
-                } else {
-                    return `An error has occured (${response_weather.status}) when fetching on the openweathermap api.`;
-                }
+        const response_weather = await fetch(cst.openweather.api_url + `lat=${node.latitude}&lon=${node.longitude}&appid=${cst.openweather.key}`);
+
+        if (!response_weather.ok) {
+            if (response_weather.status == 401) {
+                return `Error: You need to input an an API key in the file: mer-b/back/constants.json`;
+            } else {
+                console.log(response_weather)
+                return `An error has occured (${response_weather.status}) when fetching on the openweathermap api.`;
             }
-
-            const data_weather = await response_weather.json();
-
-            console.log(data_weather);
-
-            const unix_sunrise = data_weather.sys.sunrise;
-            const unix_sunset = data_weather.sys.sunset;
-            const unix_actualTime = data_weather.dt;
-
-            function time(unix) {
-                let time = new Date(unix * 1000);
-                let hours = time.getHours();
-                let minutes = "0" + time.getMinutes();
-                let seconds = "0" + time.getSeconds();
-                return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-            }
-
-
-            node.weather = {
-                sky: data_weather.weather[0].main,
-                temp: data_weather.main.temp -273.15, // From Kelvin to Celcius
-                wind: data_weather.wind.speed,
-            };
-
-            node.time = {
-                actualTime: time(unix_actualTime),
-                aube: time(unix_sunrise - 3600), // 1 hour before sunrise is "aube"
-                creneauAube: [time(unix_sunrise - 5400), time(unix_sunrise + 5400)],
-                crepuscule: time(unix_sunset + 3600), // 1 hour after sunset is "crepuscule",
-                creneauCrepuscule: [time(unix_sunset - 5400), time(unix_sunset + 5400)]
-            };
         }
+
+        const data_weather = await response_weather.json();
+
+        // format the information
+        const prediction = [];
+
+        const prediction_hourly = data_weather.hourly;
+        const prediction_daily = data_weather.daily;
+
+        for (const p of prediction_hourly) {
+            prediction.push({
+                time: p.dt,
+                temperature: p.temp,
+                feels_like: p.feels_like,
+                weather: p.weather.main,
+                sunrise: prediction_daily[Math.floor(prediction_hourly.indexOf(p)/24)].sunrise,
+                sunset: prediction_daily[Math.floor(prediction_hourly.indexOf(p)/24)].sunset
+            });
+        }
+
+        for (let i = 2; i<prediction_daily.length; i++) {
+            prediction.push({
+                time: prediction_daily[i].dt,
+                temperature: prediction_daily[i].temp,
+                feels_like: prediction_daily[i].feels_like,
+                weather: prediction_daily[i].weather.main,
+                sunrise: prediction_daily[i].sunrise,
+                sunset: prediction_daily[i].sunset
+            });
+        }
+
+        weather.push(prediction);
     }
+    
+    return weather;
+
+    // if (filtres.hasOwnProperty("weather") || filtres.hasOwnProperty("time") || filtres.hasOwnProperty("sea")) {
+
+        
+
+    //         const data_weather = await response_weather.json();
+
+            
+
+    //         return data_weather;
+    //         console.log(data_weather);
+
+    //         const unix_sunrise = data_weather.sys.sunrise;
+    //         const unix_sunset = data_weather.sys.sunset;
+    //         const unix_actualTime = data_weather.dt;
+
+    //         function time(unix) {
+    //             let time = new Date(unix * 1000);
+    //             let hours = time.getHours();
+    //             let minutes = "0" + time.getMinutes();
+    //             let seconds = "0" + time.getSeconds();
+    //             return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    //         }
+
+
+    //         node.weather = {
+    //             sky: data_weather.weather[0].main,
+    //             temp: data_weather.main.temp -273.15, // From Kelvin to Celcius
+    //             wind: data_weather.wind.speed,
+    //         };
+
+    //         node.time = {
+    //             actualTime: time(unix_actualTime),
+    //             aube: time(unix_sunrise - 3600), // 1 hour before sunrise is "aube"
+    //             creneauAube: [time(unix_sunrise - 5400), time(unix_sunrise + 5400)],
+    //             crepuscule: time(unix_sunset + 3600), // 1 hour after sunset is "crepuscule",
+    //             creneauCrepuscule: [time(unix_sunset - 5400), time(unix_sunset + 5400)]
+    //         };
+    //     }
+    // }
 
     // filter
     if (filtres.hasOwnProperty("weather")) {
