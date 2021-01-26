68145e0817e50f4c431752db49c2a4b5c47a498e
Antoine Garnier <antoine.garnier1@etu.univ-nantes.fr>
2021-01-26 15:49:51 +0100

    Ajout de ButtonCustom + styles

--- boulot-b/frontend/src/components/Answer.vue
+++ boulot-b/frontend/src/components/Answer.vue
@@ -1,18 +1,44 @@
 <template>
-  <div>
+  <div class="answers">
     <div v-for="answer in answers" :key="answer">
-      <p> {{ answer }} </p>
+      <buttonCustom v-bind:text="answer" v-bind:color=chooseColor(color)></buttonCustom>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import ButtonCustom from "../components/ButtonCustom.vue"
+
 export default Vue.component("answer", {
   name: "answer",
   props: ["answers"],
+  components: {
+    ButtonCustom
+  },
+
+  methods: {
+    chooseColor() {
+      if (this.color === 'yellow') {
+        this.color = 'blue';
+        return 'yellow';
+      }
+      this.color = 'yellow';
+      return 'blue';
+    }
+  },
+  
+  data() {
+    return {
+      color: 'yellow'
+    }
+  }
 });
 </script>
 
 <style scoped>
+  .answers {
+    display: inline;
+    position: relative;
+  }
 </style>
---
+++ boulot-b/frontend/src/components/ButtonCustom.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-on:click="click(text)" v-bind:id=color class="button">
+    {{ text }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.component("buttonCustom", {
+  name: "buttonCustom",
+  props: ['color', 'text'],
+  methods: {
+    click(text) {
+      this.$emit("getChoice", text)
+    }
+  }
+});
+</script>
+
+<style scoped>
+  .button {
+    display: block;
+    position: relative;
+
+    text-align: center;
+    margin-bottom: 5px;
+  }
+
+  #yellow {
+    background-color: yellow;
+  }
+
+  #blue {
+    background-color: #0ec0ec;
+  }
+</style>
