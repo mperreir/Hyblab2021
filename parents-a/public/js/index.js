b799848a520d3a83fee60b7e8966bec85ff6718f
Antoine Zuber <antoine.zuber@etu.univ-nantes.fr>
2021-01-26 16:14:20 +0100

    Initialisation du slide avec la premiere slide

--- parents-a/public/js/index.js
+++ parents-a/public/js/index.js
@@ -13,5 +13,18 @@ let mySlidr = slidr.create('slidr',{
     transition: 'fade'
   }).start();
 
+console.log("Index")
+
 // on s'occupe de la 1ère slide
+let initSlide1 = function() {
+  d3.select('#logo').on('click', function(){
+      anime({
+          targets: '#logo',
+          scale: 0
+        });
+      mySlidr.slide('page-2');
+      initSlide2();
+    });
+}
+
 initSlide1();
