a9a7c303742647cdf01390fa8b7613423c22f616
Théo BOUAS <theo.bouas@etu.univ-nantes.fr>
2021-01-26 14:18:46 +0100

    [AJOUT] addresstocoordinates() | THEO BOUAS

---
+++ proximite-a/api/adresstocoordinates.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let addresstocoordinates = async function(adresse){
+    let adresse2 = adresse.replaceAll(' ', '+');
+    console.log(adresse2);
+    const url = `https://api-adresse.data.gouv.fr/search/?q=${adresse2}`;
+    console.log(url);
+    let response =await fetch(`https://api-adresse.data.gouv.fr/search/?q=${adresse2}`);
+    let data = await response.json();
+    let coordinates = {latitude: data.features[0].geometry.coordinates[1], longitude: data.features[0].geometry.coordinates[0]}
+    return coordinates;
+}
+
+let result = await addresstocoordinates('41 boulevard albert einstein nantes france');
+console.log(result);
+
+export default addresstocoordinates;
