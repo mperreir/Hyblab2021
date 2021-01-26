abf2d1d63583ab89c782fc1b1b894e091ad28ddf
Efflam SIMON <efflam.simon@etu.univ-nantes.fr>
2021-01-26 13:58:45 +0100

    Add more routes to api

--- velo-b/api/api.js
+++ velo-b/api/api.js
@@ -16,9 +16,19 @@ module.exports = () => {
     // création d'une app express
     const app = express();
     
-    // routes
-    app.get('/abris-velos/:quartier', JsonRoute((req,res) => loadJSONFile('abris-velos-nantes-metropole.json')[req.params['quartier']]));
-    app.get('/test/',JsonRoute((req, res) => loadJSONFile('abris-velos-nantes-metropole.json')));
+    // routes depuis les fichiers json
+    app.get('/abris-velos/:quartier', JsonRoute((req,res) => loadJSONFile('abris-velos.json')[req.params['quartier']]));
+    app.get('/amenagements-cyclables/:quartier', JsonRoute((req,res) => loadJSONFile('amenagements-cyclables.json')[req.params['quartier']]));
+    app.get('/gonfleurs-libre-service/:quartier', JsonRoute((req,res) => loadJSONFile('gonfleurs-libre-service.json')[req.params['quartier']]));
+    app.get('/services-velos-bicloo/', JsonRoute((req,res) => loadJSONFile('services-velos-bicloo.json')));
+    app.get('/stations-velos-libre-service/:quartier', JsonRoute((req,res) => loadJSONFile('stations-velos-libre-service.json')[req.params['quartier']]));
+    app.get('/tan-arrets/:quartier', JsonRoute((req,res) => loadJSONFile('tan-arrets.json')[req.params['quartier']]));
+    app.get('/velocistes/:quartier', JsonRoute((req,res) => loadJSONFile('velocistes.json')[req.params['quartier']]));
+    
+    // routes depuis l'api de nantes metropole
+    //app.get('/parcs-relais-disponibilites/:quartier', JsonRoute((req,res) => TODO ));
+    //app.get('/parkings-publics-disponibilites/:quartier', JsonRoute((req,res) => TODO ));
+    //app.get('/stations-velos-libre-service-disponibilites/:quartier', JsonRoute((req,res) => TODO ));
     
     // utilitaire pour créer une route qui envoie du json
     function JsonRoute(callback) {
