42e7bae315890d45906bb2aad72f68b7cb64037c
Paul BLANLOEIL <paul.blanloeil@etu.univ-nantes.fr>
2021-01-26 11:21:46 +0100

    Optimisation du polygone (pas besoin de la distance)

--- proximite-a/api/getZone.js
+++ proximite-a/api/getZone.js
@@ -5,7 +5,7 @@ var request = require('request');
 request({
   method: 'POST',
   url: 'https://api.openrouteservice.org/v2/isochrones/foot-walking',
-  body: '{"locations":[[-1.55,47.2]],"range":[900,10000]}',
+  body: '{"locations":[[-1.55,47.2]],"range":[900,1]}',
   headers: {
     'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
     'Authorization': '5b3ce3597851110001cf624829bf42551469445aa00ca476f174c648',
