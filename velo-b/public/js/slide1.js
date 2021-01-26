0c5724188731ab7aed9261991f54216426b7486e
theophane fouillard <theophane.fouillard@etu.univ-nantes.fr>
2021-01-26 11:05:07 +0100

    import svg choice 4

--- velo-b/public/js/slide1.js
+++ velo-b/public/js/slide1.js
@@ -1,42 +1,40 @@
-let initSlide1 = function () {
+let initSlide1 = function(){
 
-    d3.select('#logo-hyblab').on('click', function () {
-        anime({
-            targets: '#logo-hyblab',
-            scale: 0
-        });
-        mySlidr.slide('page-2');
-        initSlide2();
-    });
-
-    d3.select('#logo-hyblab').on('mouseover', function () {
-        anime({
-            targets: '#logo-hyblab',
-            scale: 1.2
-        });
-    });
-
-    d3.select('#logo-hyblab').on('mouseout', function () {
-        anime({
-            targets: '#logo-hyblab',
-            scale: 1
-        });
-    });
-
+  d3.select('#logo-hyblab').on('click', function(){
     anime({
-        targets: '#titre',
-        translateX: '100%',
-        easing: 'easeInOutQuad',
-        direction: 'alternate',
-        loop: true
+        targets: '#logo-hyblab',
+        scale: 0
+      });
+    mySlidr.slide('choix-transport-4');
+    initSlide3();
+  });
+
+  d3.select('#logo-hyblab').on('mouseover', function(){
+    anime({
+        targets: '#logo-hyblab',
+        scale: 1.2
+      });
+  });
+
+  d3.select('#logo-hyblab').on('mouseout', function(){
+    anime({
+        targets: '#logo-hyblab',
+        scale: 1
+      });
+  });
+
+  anime({
+    targets: '#titre',
+    translateX: '100%',
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+  });
+
+  d3.json('data/dummy.json')
+    .then(function(data) {
+        d3.select('footer')
+            .html('p')
+            .text(data.message);
     });
-
-    d3.json('data/dummy.json')
-        .then(function (data) {
-            d3.select('footer')
-                .html('p')
-                .text(data.message);
-        });
 };
-
-
---
+++ velo-b/public/js/slide3.js
@@ -0,0 +1,35 @@
+let initSlide3 = function(){
+
+  d3.select('#ok-button').on('click', function(){
+    anime({
+        targets: 'ok-button',
+        scale: 0
+      });
+    mySlidr.slide('page-2');
+    initSlide2();
+  });
+
+  d3.select('#fleche-droite').on('mouseover', function(){
+    anime({
+        targets: '#fleche-droite',
+        scale: 1.2
+      });
+  });
+
+  d3.select('#fleche-droite').on('mouseout', function(){
+    anime({
+        targets: '#fleche-droite',
+        scale: 1
+      });
+  });
+
+  anime({
+    targets: '#transport-4',
+    translateX: '100%',
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+  });
+
+  
+};
