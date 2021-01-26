1ec9cb8f56989e2a81c5f668f00bfcc1008f2253
Paul BLANLOEIL <paul.blanloeil@etu.univ-nantes.fr>
2021-01-26 15:46:12 +0100

    Traduction de la méthode zone en async/await et fetch

--- proximite-a/api/getZone.js
+++ proximite-a/api/getZone.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const request = require('request');
+const fetch = require('node-fetch');
 
 /**
  * Renvoie un polygone définissant ce qui peut être atteint
@@ -13,27 +13,24 @@ const request = require('request');
  * "cycling-regular", "cycling-electric" ou "cycling-road"
  * en ce qui nous concerne.
  */
-function get15mnZone(position = [-1.55, 47.21], modeTransport = 'foot-walking') {
-    let data;
-    /*
-    request({
-        method: 'POST',
-        url: 'https://api.openrouteservice.org/v2/isochrones/' + modeTransport,
-        body: '{"locations":[['+position+']],"range":[900,1]}',
-        headers: {
-            'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
-            'Authorization': '5b3ce3597851110001cf624829bf42551469445aa00ca476f174c648',
-            'Content-Type': 'application/json; charset=utf-8'
-        }}, function (error, response, body) {
-            console.log('Status:', response.statusCode);
-            console.log('Headers:', JSON.stringify(response.headers));
-            console.log('Response:', body);
-            data = body;
-    });
-    */
-    data = require('./test_ors.json');
-    
-    let polygon = data.features[0].geometry.coordinates;
-    return polygon;
+let get15mnZone = async function(position, modeTransport = 'foot-walking') {
+    try {
+        const data = await fetch(`https://api.openrouteservice.org/v2/isochrones/${modeTransport}`, {
+            method: 'POST',
+            body: `{"locations":[[${position}]],"range":[900,1]}`,
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': '5b3ce3597851110001cf624829bf42551469445aa00ca476f174c648',
+                'Content-Type': 'application/json'
+            }});
+        //data = require('./test_ors.json');
+        
+        let polygon = data.features[0].geometry.coordinates;
+        return polygon;
+    } catch (e) {
+        console.error(e);
+    }
 }
+
+module.exports = {get15mnZone};
 
