64ce12fee48cd8b9d767848b584ae2ff8b9a62df
Théo BOUAS <theo.bouas@etu.univ-nantes.fr>
2021-01-26 16:02:37 +0100

    [AJOUT] centralisation des fetch dans request.js | THEO BOUAS

--- proximite-a/api/adresstocoordinates.js
+++ proximite-a/api/adresstocoordinates.js
@@ -1,17 +1,17 @@
 'use strict'
+const request = require('./request');
+
 
 let addresstocoordinates = async function(adresse){
-    let adresse2 = adresse.replaceAll(' ', '+');
-    console.log(adresse2);
-    const url = `https://api-adresse.data.gouv.fr/search/?q=${adresse2}`;
-    //console.log(url);
-    let response =await fetch(url);
+    let adresse2 = adresse.replace(/\s/g, '+');
+    const url = `http://api-adresse.data.gouv.fr/search/?q=${adresse2}`;
+    let response =await request.request(url);
     let data = await response.json();
     let coordinates = {latitude: data.features[0].geometry.coordinates[1], longitude: data.features[0].geometry.coordinates[0]}
     return coordinates;
 }
 
-let result = await addresstocoordinates('41 boulevard albert einstein nantes france');
-//console.log(result);
-
-export default addresstocoordinates;
+/*let result =  addresstocoordinates('41 boulevard albert einstein nantes france')
+    .then(data => {console.log(data)});
+*/
+exports.addresstocoordinates = addresstocoordinates;
