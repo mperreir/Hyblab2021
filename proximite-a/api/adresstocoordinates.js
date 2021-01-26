42201436e6f7b82a3ea6166dba1e3f3f1918a07c
Théo BOUAS <theo.bouas@etu.univ-nantes.fr>
2021-01-26 14:23:49 +0100

    [FIX] mise en commentaire des console.log() | THEO BOUAS

--- proximite-a/api/adresstocoordinates.js
+++ proximite-a/api/adresstocoordinates.js
@@ -4,14 +4,14 @@ let addresstocoordinates = async function(adresse){
     let adresse2 = adresse.replaceAll(' ', '+');
     console.log(adresse2);
     const url = `https://api-adresse.data.gouv.fr/search/?q=${adresse2}`;
-    console.log(url);
-    let response =await fetch(`https://api-adresse.data.gouv.fr/search/?q=${adresse2}`);
+    //console.log(url);
+    let response =await fetch(url);
     let data = await response.json();
     let coordinates = {latitude: data.features[0].geometry.coordinates[1], longitude: data.features[0].geometry.coordinates[0]}
     return coordinates;
 }
 
 let result = await addresstocoordinates('41 boulevard albert einstein nantes france');
-console.log(result);
+//console.log(result);
 
 export default addresstocoordinates;
