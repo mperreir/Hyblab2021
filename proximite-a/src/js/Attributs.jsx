bc576f2b1875c269318ecbc18a3ce42b1776fe98
Ayoub NAJIM <polynesse@protonmail.ch>
2021-01-26 14:03:49 +0000

    ...

--- proximite-a/src/js/Attributs.jsx
+++ proximite-a/src/js/Attributs.jsx
@@ -4,16 +4,18 @@ class Attributs extends  React.Component {
     render() {
         const {onNextPage, onPreviousPage} = this.props;
         return (
-            <div>
+            <div id="attribusContainer2" >
+                <div class="d-flex flex-column justify-content-end ">
                 <h3>Choisi ton point de départ</h3>
                 <input type='button' value='Activer ma géolocalisation'/>
                 <label type='button'> Choisir une adresse </label>
                 <input type='text' placeholder=''/>
                 <input type='button' value='valider' onClick={onNextPage}/>
-
-
+                <br />
+                <br />
                 <input type='button' value='←' onClick={onPreviousPage}/>
                 <input type='button' value='→' onClick={onNextPage}/>
+                </div>
             </div>
         );
     }
