6c4c7bf696944683cac0bfe60ffa7df4192076e0
PaulEvans8669 <amatokus8669@gmail.com>
2021-01-26 12:46:43 +0100

    Ajout de l'updater des données

--- velo-b/api/api.js
+++ velo-b/api/api.js
@@ -3,6 +3,7 @@
 // import des module Utiles
 const express = require('express');
 const fs = require('fs');
+const fetch = require('node-fetch');
 
 function loadJSONFile(file_name){
     let rawdata = fs.readFileSync(`./velo-b/api/data/${file_name}`);
@@ -15,11 +16,11 @@ module.exports = () => {
 
     // création d'une app express
     const app = express();
-    
+
     // routes
     app.get('/abris-velos/:quartier', JsonRoute((req,res) => loadJSONFile('abris-velos-nantes-metropole.json')[req.params['quartier']]));
     app.get('/test/',JsonRoute((req, res) => loadJSONFile('abris-velos-nantes-metropole.json')));
-    
+    app.get('/update/', update);
     // utilitaire pour créer une route qui envoie du json
     function JsonRoute(callback) {
         return function(req, res) {
@@ -29,6 +30,22 @@ module.exports = () => {
             });
             res.send(JSON.stringify(callback(req, res)));
         };
+    }
+
+    function update(req, res, next) {
+        const api_routes = loadJSONFile('nantes-api-fetcher.json');
+        const quartiers = loadJSONFile('quartiers.json');
+        api_routes.forEach(async o => {
+            const data = {};
+            for (const k of Object.keys(quartiers)) {
+                const polygon_api = quartiers[k].reduce((acc, val) => acc + `(${val[0]}%2C${val[1]})%2C`,'').slice(0, -3);
+                const url = o.route + '&geofilter.polygon=' + polygon_api;
+                const response = await fetch(url);
+                data[k] = await response.json();
+            }
+            fs.writeFileSync(`./velo-b/api/data/${o.fileName}`, JSON.stringify(data));
+        })
+        res.send('Done');
     }
 
     return app;
