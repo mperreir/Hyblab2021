53e463e88385609db9c6d4221df481557dfdc6a8
Andréa Gugole <andrea.gugole@etu.univ-nantes.fr>
2021-01-26 14:50:43 +0100

    ajout logos + image theme

--- proximite-a/src/js/ThemeSelector.jsx
+++ proximite-a/src/js/ThemeSelector.jsx
@@ -1,52 +1,57 @@
 import React from 'react';
 import logo from '../img/LOGO OK_logo principal.png'
+import fetard_img from '../img/Perso-hyblab-03.png'
+import sportif_img from '../img/Perso-hyblab-02.png'
+import gourmet_img from '../img/Perso-hyblab-04.png'
+import curieux_img from '../img/Perso-hyblab-05.png'
+import famille_img from '../img/Perso-hyblab-06.png'
 import '../css/theme.css'
-class ThemeSelector extends  React.Component {
+class ThemeSelector extends React.Component {
     render() {
-        const {onNextPage, onPreviousPage} = this.props;
+        const { onNextPage, onPreviousPage } = this.props;
         return (
             <div className="bg-info m-5 d-flex justify-content-center align-items-center flex-column">
                 <h3>Tu est plutot .. ?</h3>
                 <div className="container">
                     <div className="row">
                         <div className="col">
-                            <input type="radio" name="theme" id="fetard" className="input-hidden"/>
+                            <input type="radio" name="theme" id="fetard" className="input-hidden" />
                             <label htmlFor="fetard">
-                                <img src="//placekitten.com/150/150" alt="fetard"/>
+                                <img src={fetard_img} alt="fetard" />
                             </label>
                         </div>
 
                         <div className="col">
-                            <input type="radio" name="theme" id="sportif" className="input-hidden"/>
+                            <input type="radio" name="theme" id="sportif" className="input-hidden" />
                             <label htmlFor="sportif">
-                                <img src="//placekitten.com/151/151" alt="sportif"/>
+                                <img src={sportif_img} alt="sportif" />
                             </label>
                         </div>
 
                         <div className="col">
-                            <input type="radio" name="theme" id="gourmet" className="input-hidden"/>
+                            <input type="radio" name="theme" id="gourmet" className="input-hidden" />
                             <label htmlFor="gourmet">
-                                <img src="//placekitten.com/147/147" alt="gourmet"/>
+                                <img src={gourmet_img} alt="gourmet" />
                             </label>
                         </div>
 
                         <div className="col">
-                            <input type="radio" name="theme" id="curieux" className="input-hidden"/>
+                            <input type="radio" name="theme" id="curieux" className="input-hidden" />
                             <label htmlFor="curieux">
-                                <img src="//placekitten.com/148/148" alt="curieux"/>
+                                <img src={curieux_img} alt="curieux" />
                             </label>
                         </div>
 
                         <div className="col">
-                            <input type="radio" name="theme" id="famille" className="input-hidden"/>
+                            <input type="radio" name="theme" id="famille" className="input-hidden" />
                             <label htmlFor="famille">
-                                <img src="//placekitten.com/149/149" alt="familley"/>
+                                <img src={famille_img} alt="familley" />
                             </label>
                         </div>
                     </div>
                 </div>
                 <span className="m-2 bg-warning p-2">fast food - bars - boîtes ....</span>
-                <input type='button' className="btn btn-primary mb-3" value='Valider' onClick={onNextPage}/>
+                <input type='button' className="btn btn-primary mb-3" value='Valider' onClick={onNextPage} />
             </div>
         );
     }
