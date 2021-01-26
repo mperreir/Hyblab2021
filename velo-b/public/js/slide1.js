b5f115b1c8e8e2aadc2c5c96fa18406a8772a2e9
theophane fouillard <theophane.fouillard@etu.univ-nantes.fr>
2021-01-26 16:41:52 +0100

    add missing elements due to merge error

--- velo-b/public/js/slide1.js
+++ velo-b/public/js/slide1.js
@@ -1,40 +1,40 @@
-let initSlide1 = function(){
+let initSlide1 = function () {
 
-  d3.select('#logo-hyblab').on('click', function(){
-    anime({
-        targets: '#logo-hyblab',
-        scale: 0
+  d3.select('#startButton').on('click', function () {
+      anime({
+          targets: '#startButton',
+          scale: 0
       });
-    mySlidr.slide('choix-transport-1');
-    initSlideChoixVoiture();
+      mySlidr.slide('choix-transport-1');
+      initSlideChoixVoiture();
   });
 
-  d3.select('#logo-hyblab').on('mouseover', function(){
-    anime({
-        targets: '#logo-hyblab',
-        scale: 1.2
+  d3.select('#startButton').on('mouseover', function () {
+      anime({
+          targets: '#startButton',
+          scale: 1.2
       });
   });
 
-  d3.select('#logo-hyblab').on('mouseout', function(){
-    anime({
-        targets: '#logo-hyblab',
-        scale: 1
+  d3.select('#startButton').on('mouseout', function () {
+      anime({
+          targets: '#startButton',
+          scale: 1
       });
   });
 
   anime({
-    targets: '#titre',
-    translateX: '100%',
-    easing: 'easeInOutQuad',
-    direction: 'alternate',
-    loop: true
+      targets: '#titre',
+      translateX: '100%',
+      easing: 'easeInOutQuad',
+      direction: 'alternate',
+      loop: true
   });
 
   d3.json('data/dummy.json')
-    .then(function(data) {
-        d3.select('footer')
-            .html('p')
-            .text(data.message);
-    });
+      .then(function (data) {
+          d3.select('footer')
+              .html('p')
+              .text(data.message);
+      });
 };
