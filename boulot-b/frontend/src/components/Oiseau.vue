2f51ec9ec547bec0a4e885228a000bc1029ccf3c
Friviere <florent.riviere@etu.univ-nantes.fr>
2021-01-26 17:03:16 +0100

    ajout bulle texte

--- boulot-b/frontend/src/components/Oiseau.vue
+++ boulot-b/frontend/src/components/Oiseau.vue
@@ -4,8 +4,31 @@
 <!-- <lottie-animation
     path="/assets/animationJson/wazo_culture.json"
 /> -->
-<div id="wazo-anim" style=" width: 20%;
- "></div>
+<div style="position:absolute;">
+    <div id="bulle-group" style="position:relative;width: 300px;" >  
+     
+
+    <img src="../assets/bulle/bulle4x.png" style="position:relative; width:100%">
+    <img src="../assets/bulle/fleche4x.png" style="position:absolute;  left : 60% ; top: 70%  ;width:40%;">
+
+    
+      <div class="centered" style="position: absolute;  top: 40%;  left: 50%;  transform: translate(-50%, -50%); width: 250px; height: 100px; word-wrap: break-word ">
+        <p style="   font-family: Poppins;
+font-size: 15px;
+font-style: normal;
+font-weight: 600;
+line-height: 30px;
+letter-spacing: 0em;
+text-align: left;
+">AAAAAAAAaaAAAAAAAAAAAAAAAAaaAAAAAAAAaaAAAAAAAAAAAAAAAAAAAAAAAAAA
+          </p>
+    </div>
+  </div>
+<div id="wazo-anim" style="position:relative; width: 20%; left:30%">
+  </div>
+
+
+</div>
 
 
 </template>
@@ -17,7 +40,7 @@ import lottie from "lottie-web";
 
 export default {
   name: "Oiseau",
-  props :['anim'], // on change l'anim en fonction de l'écran/ choix faits / remplace l'import.
+  props :['anim','message'], // on change l'anim en fonction de l'écran/ choix faits / remplace l'import.
         mounted () {
         animationData().then(function(data) { // data contient les données du json
         lottie.loadAnimation({
