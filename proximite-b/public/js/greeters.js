25430c4a4ca12891753d80240816d7b4b84fad9d
Manon BAILLET <manon.baillet@etu.univ-nantes.fr>
2021-01-26 21:42:50 +0100

    fix css pb (toujours pas fou sur ecran 15pouces)

--- proximite-b/public/js/greeters.js
+++ proximite-b/public/js/greeters.js
@@ -2,19 +2,19 @@ function greeter_background(){
     persona = read_store('personas').chosen;
 
     if (persona == "jeune"){
-        $('#index_content_greeter').css("background-image", "url('/proximite-b/img/greeter/sans_la_bulle_jeune.svg')");
+        $('#greeter_content').css("background-image", "url('/proximite-b/img/greeter/sans_la_bulle_jeune.svg')");
         $('#persona_text').text("Bonjour Monsieur, vous avez raison, je ne connais pas très bien la ville. Je suis venu visiter avant de m'installer ici, mais par où commencer ? J'hésite entre 2 quartiers");
         $('#greeter_second').text("Bonjour jeune homme, vous avez l'air un petit peu perdu. Je peux peut-être vous aider ?");
         $('.bulle_persona').css("visibility","hidden");
     }
     else if (persona == "famille"){
-        $('#index_content_greeter').css("background-image", "url('/proximite-b/img/greeter/sans_la_bulle_famille.svg')");
+        $('#greeter_content').css("background-image", "url('/proximite-b/img/greeter/sans_la_bulle_famille.svg')");
         $('#persona_text').text("Bonjour, nous venons pour visiter la ville avant de choisir où nous installer. Mais par ou commencer ? Nous ne connaissons pas vraiment la ville et nous hésitons entre 2 logements...");
         $('#greeter_second').text("Bonjour Messieurs-Dames, vous avez l'air un petit peu perdu. Je peux peut-être vous aider ?");
         $('.bulle_persona').css("visibility","hidden");
     }
     else if (persona == "senior"){
-        $('#index_content_greeter').css("background-image", "url('/proximite-b/img/greeter/sans_la_bulle_senior.svg')");
+        $('#greeter_content').css("background-image", "url('/proximite-b/img/greeter/sans_la_bulle_senior.svg')");
         $('#persona_text').text("Bonjour Monsieur, vous avez raison, je ne connais pas très biens la ville, je suis venu visiter avant de choisir ou m'installer, mais par ou commencer ? J'hésite entre 2 quartiers...");
         $('#greeter_second').text("Bonjour Madame, vous avez l'air un petit peu perdu. Je peux peut-être vous aider ?");
         $('.bulle_persona').css("visibility","hidden");
