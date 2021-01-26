44d493048dcda77e010481dd694a3cfadb49e93d
Lucille Gallet <lucille.gallet@etu.univ-nantes.fr>
2021-01-26 11:33:21 +0100

    ajout date et heure final

--- mer-b/public/js/slideFin.js
+++ mer-b/public/js/slideFin.js
@@ -73,9 +73,34 @@ function updateSlideFin(plages, criteres){
     let temperature = cartePostale.querySelector(".temperatureRelle");
     temperature.innerHTML = element.weather.temperature + '°C';
     let leverSoleil = cartePostale.querySelector(".leverSoleil");
-    leverSoleil.innerHTML = new Date(element.weather.sunrise).getHours() + ":" + new Date(element.weather.sunrise).getMinutes();
+
+    let dateLever = new Date(element.weather.sunrise);
+    if(dateLever.getMinutes() < 10){
+      leverSoleil.innerHTML = new Date(element.weather.sunrise).getHours() + ":0" + new Date(element.weather.sunrise).getMinutes();
+    }
+    else {
+      leverSoleil.innerHTML = new Date(element.weather.sunrise).getHours() + ":" + new Date(element.weather.sunrise).getMinutes();
+    }
+
     let coucherSoleil = cartePostale.querySelector(".coucherSoleil");
-    coucherSoleil.innerHTML = new Date(element.weather.sunset).getHours() + ":" + new Date(element.weather.sunset).getMinutes();
+    let datecoucher = new Date(element.weather.sunset);
+    if(datecoucher.getMinutes() < 10){
+      coucherSoleil.innerHTML = new Date(element.weather.sunset).getHours() + ":0" + new Date(element.weather.sunset).getMinutes();
+    }
+    else {
+      coucherSoleil.innerHTML = new Date(element.weather.sunset).getHours() + ":" + new Date(element.weather.sunset).getMinutes();
+    }
+
+    let date = cartePostale.querySelector("#date");
+    date.innerHTML = new Date(element.time).getDate() + '/' + (new Date(element.time).getMonth() + 1);
+
+    let heure = cartePostale.querySelector("#heure");
+    if(new Date(element.time).getMinutes() < 10){
+      heure.innerHTML = new Date(element.time).getHours() + ":0" + new Date(element.time).getMinutes();
+    }
+    else {
+      heure.innerHTML = new Date(element.time).getHours() + ":" + new Date(element.time).getMinutes();
+    }
 
     if(element.phare){
       let phare = cartePostale.querySelector(".phareNom");
