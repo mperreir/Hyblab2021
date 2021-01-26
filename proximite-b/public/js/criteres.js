3b1e1b4a7ea35d0a65b1f8fd8fdac149b01a48b7
Lysa <lysa.corcuff@etu.univ-nantes.fr>
2021-01-26 11:22:56 +0100

    Drag and drog avec priorité fonctionnel

--- proximite-b/public/js/criteres.js
+++ proximite-b/public/js/criteres.js
@@ -1,39 +1,16 @@
-function criteres_allowDrop(ev) {
-    ev.preventDefault();
-}
 
-function criteres_drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function criteres_drop(ev) {
-    ev.preventDefault();
-
-    if(ev.target.id == "div2" && ev.target.children.length < 5){
-        var data = ev.dataTransfer.getData("text");
-        ev.target.appendChild(document.getElementById(data));
-    }
-    else if(ev.target.id == "div3" && ev.target.children.length < 3){
-        var data = ev.dataTransfer.getData("text");
-        ev.target.appendChild(document.getElementById(data));
-    }else if(ev.target.id == "div1"){
-        var data = ev.dataTransfer.getData("text");
-        ev.target.appendChild(document.getElementById(data));
-    }
-
-}
 function criteres_submitInterest() {
-    var div2 = document.getElementById("div2");
-    var div3 = document.getElementById("div3");
+    var ul2 = document.getElementById("sortable2");
+    var ul3 = document.getElementById("sortable3");
     let interest = new Array();
     let disinterest = new Array();
 
-    if(div2.children.length >= 3){
-        for (let child1 of div2.children) {
+    if(ul2.children.length >= 3){
+        for (let child1 of ul2.children) {
             interest.push(child1.id);
         }
 
-        for (let child2 of div3.children) {
+        for (let child2 of ul3.children) {
             disinterest.push(child2.id);
         }
 
