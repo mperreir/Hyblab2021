abf9c490d50e2efcf9dc0088bd177ad74ee0f8d6
Clement_Hue <clementhue@hotmail.fr>
2021-01-26 11:23:15 +0100

    switch componenet

---
+++ boulot-b/frontend/src/components/Question.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>michel</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "question",
+  methods: {
+    launch() {
+      this.$emit("switch-component");
+    },
+  },
+});
+</script>
+
+<style scoped>
+</style>
