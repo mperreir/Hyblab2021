5578f2a37d3f037da21452974159c30805e82857
Manon BAILLET <manon.baillet@etu.univ-nantes.fr>
2021-01-27 15:30:06 +0100

    etudiant -> jeune & vieux -> senior

--- proximite-b/public/js/spa.js
+++ proximite-b/public/js/spa.js
@@ -84,10 +84,10 @@ const go_to = (page, data, callback) => {
                 if(read_store('personas').chosen == "famille"){
                     div_content.style.backgroundImage = "url('/proximite-b/img/criteres/background_criteres_famille.svg')";
                     console.log("test");
-                }else if(read_store('personas').chosen == "etudiant"){
+                }else if(read_store('personas').chosen == "jeune"){
                     div_content.style.backgroundImage = "url('/proximite-b/img/criteres/background_criteres_etu.svg')";
                     console.log("test2");
-                }else if(read_store('personas').chosen == "vieux"){
+                }else if(read_store('personas').chosen == "senior"){
                     console.log("test3");
                 }
                 $(function() {
