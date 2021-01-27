547ff2e7a55c6a84a3092bce19de7d78ff26ec45
theo.berard@outlook.fr <theo.berard@outlook.fr>
2021-01-27 21:58:51 +0100

    ajout fonction getBowFromZone

--- proximite-a/src/api/getZone.js
+++ proximite-a/src/api/getZone.js
@@ -1,3 +1,4 @@
+import request from './request';
 
 function getZone(position, transportation) {
     let polygon = null;
@@ -67,12 +68,16 @@ export const POINT_TYPES = [
  * @param {*} zone
  * @param {Array} types
  */
-export const getPointsInZone = (zone, types) => {
+export async function getPointsInZone(zone, types) {
     const query = buildQuery(types, zone);
+    let res = await request.request(query, null);
+    console.log(res);
+
+    return res;
 }
 
 export const buildQuery = (types, zone) => {
-    let query = "http://overpass-api.de/api/interpreter?data=[out:json];";
+    let query = "https://overpass-api.de/api/interpreter?data=[out:json];";
     let area = "name=Nantes";
     query += "area[" + area + "];("
  
@@ -89,4 +94,32 @@ export const buildQuery = (types, zone) => {
     query += ");out;";
 
     return query
+}
+
+export let zone = [
+    [
+        [-1.573835,47.264487],
+        [-1.573808,47.264132],
+        [-1.573623,47.264113],
+        [-1.573586,47.264471],
+        [-1.573771,47.26449],
+        [-1.573799,47.264492],
+        [-1.573817,47.264489],
+        [-1.573817,47.264489],
+        [-1.573835,47.264487]
+    ]
+];
+export const getBoxFromZone = (zone) => {
+    let west=180, east=-180, south=-90, north=90;
+    zone = zone[0];
+    zone.forEach(point => {
+        let longCoor = point[0];
+        west = longCoor < west ? longCoor : west;
+        east = longCoor > east ? longCoor : east;
+        let latCoor = point[1];
+        south = latCoor > south ? latCoor : south;
+        north = latCoor < north ? latCoor : north;
+    });
+
+    return [north, west, south, east];
 }
