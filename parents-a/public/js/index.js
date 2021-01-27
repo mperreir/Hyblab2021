37c2581450dd3fae67c331e7c319c6624b7fd633
Antoine Zuber <antoine.zuber@etu.univ-nantes.fr>
2021-01-28 00:19:01 +0100

    Gif home page et début page 4

--- parents-a/public/js/index.js
+++ parents-a/public/js/index.js
@@ -1,26 +1,33 @@
 // init du slider (qui peut aussi faire des fondus enchainé)
 let mySlidr = slidr.create('slidr',{
-    breadcrumbs: false,
+    breadcrumbs: true,
     controls: 'none',
     direction: 'horizontal',
-    fade: false,
+    fade: true,
     keyboard: true,
     overflow: true,
     pause: false,
     theme: '#222',
     timing: { 'fade': '0.5s ease-in' },
     touch: true,
-    transition: 'linear'
+    transition: 'fade'
   }).start();
 
 // on s'occupe de la 1ère slide
 let initSlide1 = function() {
   d3.select('#logo').on('click', function(){
-      mySlidr.slide('page-2');
-      initSlide2();
+      mySlidr.slide('page-1_1');
+      initSlide1_1();
     });
 }
 
+let initSlide1_1= function() {
+    d3.select('#t').on('click', function(){
+        mySlidr.slide('page-2');
+        initSlide2_1();
+    });
+}
+/*
 let initSlide2 = function() {
     setTimeout(suiteTraitement, 1000)
     function suiteTraitement()
@@ -28,7 +35,7 @@ let initSlide2 = function() {
         mySlidr.slide('page-2.1');
         initSlide2_1();
     }
-}
+}*/
 
 let initSlide2_1 = function() {
     d3.select('#button-p2-1').on('click', function(){
@@ -37,6 +44,16 @@ let initSlide2_1 = function() {
     });
 
 }
+
+
+let initSlide3= function() {
+    d3.select('#button-p2-1').on('click', function(){
+        mySlidr.slide('page-4');
+        initSlide4();
+    });
+
+}
+
 
 //Initialisation du diaporama 
 initSlide1();
