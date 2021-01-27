ddddfd8cf9ff4bec8d6ef2619ba2a4e0b4de0038
Théo BOUAS <theo.bouas@etu.univ-nantes.fr>
2021-01-27 16:28:10 +0100

    [AJOUT] début routage dans api.js et server.js

--- proximite-a/src/api/api.js
+++ proximite-a/src/api/api.js
@@ -6,6 +6,11 @@ module.exports = function () {
 
     const app = new express();
 
-    
+    app.get('/home', function(req, res){
+        console.log('brie');
+        res.send('henlo my fren');
+    })
 
-}
+    return app
+
+};
