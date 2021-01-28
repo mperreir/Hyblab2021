aa01bca55c2f45538507ede51393915334c378df
Ayoub NAJIM <polynesse@protonmail.ch>
2021-01-28 19:23:12 +0000

    Attributs page

--- proximite-a/src/js/Home.jsx
+++ proximite-a/src/js/Home.jsx
@@ -6,11 +6,12 @@ class Home extends  React.Component {
         const {onNextPage} = this.props;
         return (
             <div id="homeContainer" class="container d-flex justify-content-center align-items-center flex-column">
+                
                 <img src={logo} width='200'></img>
                 <span class="text-center">Bienvenue sur EnvironNantes ! Cette application te permettra de découvrir tout ce qu’il y a autour de toi dans un rayon d’un quart d’heure ! </span>
                 <h2>Comment t’appelles-tu ?</h2>
-                <input type='text' placeholder='entrez votre prénom'/>
-                <input type='button' value='Explorer' onClick={onNextPage}/>
+                <input id="inputprenom" type='text' placeholder='entrez votre prénom'/>
+                <input id="explorer" type='button' value='Explorer' onClick={onNextPage}/>
             </div>
         );
     }
