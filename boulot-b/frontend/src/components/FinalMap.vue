be65f8a6a6b23de0032fefb2cd0c5ca0501315d8
Antoine Garnier <antoine.garnier1@etu.univ-nantes.fr>
2021-01-28 20:05:30 +0100

    Origin destination et via mis en paramètres

--- boulot-b/frontend/src/components/FinalMap.vue
+++ boulot-b/frontend/src/components/FinalMap.vue
@@ -9,58 +9,72 @@ import Vue from "vue";
 
 export default Vue.component("finalMap", {
   name: "finalMap",
+  props: ['origin', 'destination', 'stops'],
   mounted: function () {
-    console.log(this.$refs.mapCont)
-
     var platform = new H.service.Platform({
       apikey: 'joMJEQ1I4K91vF4CAijYMD-cvtabfFAY-iHttZRSnto'
     });
     var defaultLayers = platform.createDefaultLayers();
 
+    console.log(this.origin[0] + "," + this.origin[1]);
+    console.log(this.destination[0] + ',' + this.destination[1]);
+
     var map = new H.Map(this.$refs.mapCont,
     defaultLayers.vector.normal.map,{
     center: {lat:47.218371, lng:-1.553621},
-    zoom: 13,
+    zoom: 12,
     pixelRatio: window.devicePixelRatio || 1
     });
     window.addEventListener('resize', () => map.getViewPort().resize());
 
+    // STYLE MAP
+    // var provider = map.getBaseLayer().getProvider();
+    // var style = new H.map.Style("../assets/map/wazo_map.yaml",
+    // 'https://js.api.here.com/v3/3.1/styles/omv/');
+    // provider.setStyle(style);
     
     const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
     const ui = H.ui.UI.createDefault(map, defaultLayers);
 
-    calculateRouteFromAtoB(platform, map);
+  
+    const coordStops = this.stops.map(coords => coords[0] + "," + coords[1])
+
+    console.log(coordStops);
+
+    calculateRouteFromAtoB(platform, map, this.origin, this.destination, coordStops);
   }
 });
 
-function calculateRouteFromAtoB (platform, map) {
+function calculateRouteFromAtoB (platform, map, origin, destination, coordStops) {
+  const coordOrigin = origin[0] + ',' + origin[1];
+  const coordDestination = destination[0] + ',' + destination[1];
+  
   var router = platform.getRoutingService(null, 8),
       routeRequestParams = {
         routingMode: 'fast',
         transportMode: 'pedestrian',
-        origin: '51.51326,-0.0968752', // St Paul's Cathedral
-        destination: '51.5081,-0.0985',  // Tate Modern
-        via:'51.51148,-0.09627', // point intermédiaire random
+        origin: coordOrigin, 
+        destination: coordDestination,  
+        via: new H.service.Url.MultiValueQueryParameter(coordStops),
         return: 'polyline,travelSummary'
       };
 
 
   router.calculateRoute(
     routeRequestParams,
-    (result) => onSuccess(result, map),
+    (result) => onSuccess(result, map, origin, destination),
     onError
   );
 }
 
-function onSuccess(result, map) {
-  console.log(result)
+function onSuccess(result, map, origin, destination) {
   var route = result.routes[0];
  /*
   * The styling of the route response on the map is entirely under the developer's control.
   * A representitive styling can be found the full JS + HTML code of this example
   * in the functions below:
   */
-  addRouteShapeToMap(route, map);
+  addRouteShapeToMap(route, map, origin, destination);
   //addManueversToMap(route);
   //addManueversToPanel(route);
   //addSummaryToPanel(route);
@@ -71,7 +85,7 @@ function onError(error) {
   alert('Can\'t reach the remote server');
 }
 
-function addRouteShapeToMap(route, map){
+function addRouteShapeToMap(route, map, origin, destination){
   route.sections.forEach((section) => {
     // decode LineString from the flexible polyline
     let linestring = H.geo.LineString.fromFlexiblePolyline(section.polyline);
@@ -80,16 +94,17 @@ function addRouteShapeToMap(route, map){
     let polyline = new H.map.Polyline(linestring, {
       style: {
         lineWidth: 4,
-        strokeColor: 'rgba(0, 128, 255, 0.7)'
+        //strokeColor: 'rgba(231, 240, 13, 0.7)'
+        strokeColor: 'rgba(0, 0, 0, 1)'
       }
     });
 
     // Add the polyline to the map
     map.addObject(polyline);
     // And zoom to its bounding rectangle
-    map.getViewModel().setLookAtData({
-      bounds: polyline.getBoundingBox()
-    });
+    // map.getViewModel().setLookAtData({
+    //   bounds: polyline.getBoundingBox()
+    // });
   });
 }
 
@@ -101,15 +116,15 @@ function addRouteShapeToMap(route, map){
     position: relative;
 
     left: 30%;
-    width: 700px;
-    height: 400px;
+    width: 900px;
+    height: 600px;
   }
 
   #mapContainer {
     display: block;
     position: relative;
 
-    width: 500px;
-    height: 300px;
+    width: 700px;
+    height: 500px;
   }
 </style>
