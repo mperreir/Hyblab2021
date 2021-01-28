ad695cf2b8d421b3dc79c8a6d9c11b765688fbed
Alper OZKAN <alper.ozkan@etu.univ-nantes.fr>
2021-01-28 14:38:01 +0100

    First close button

--- velo-b/public/js/choix.js
+++ velo-b/public/js/choix.js
@@ -62,8 +62,110 @@ let initSlideChoixVelo = function () {
   arrowbutton(idFlecheD, nextD);
   arrowbutton(idFlecheG, nextG);
 
-  /*====================Bouton du bas ===========================*/
-  let infoChoix = 'infos-choix-velo';
+    d3.select('#fleche-droite-velo').on('click', function () {
+      anime({
+        targets: '#fleche-droite-velo',
+        scale: 0
+      });
+      mySlidr.slide('choix-transport-3');
+      initSlideChoixBicloo();
+    });
+  
+  
+    d3.select('#fleche-droite-velo').on('mouseover', function () {
+      anime({
+        targets: '#fleche-droite-velo',
+        scale: 1.2
+      });
+    });
+  
+    d3.select('#fleche-droite-velo').on('mouseout', function () {
+      anime({
+        targets: '#fleche-droite-velo',
+        scale: 1
+      });
+  
+    });
+  
+  
+    d3.select('#fleche-gauche-velo').on('click', function () {
+      anime({
+        targets: '#fleche-gauche-velo',
+        scale: 0
+      });
+      mySlidr.slide('choix-transport-1');
+      initSlideChoixVoiture();
+    });
+  
+    d3.select('#fleche-gauche-velo').on('mouseover', function () {
+      anime({
+        targets: '#fleche-gauche-velo',
+        scale: 1.2
+      });
+  
+    });
+  
+    d3.select('#fleche-gauche-velo').on('mouseout', function () {
+      anime({
+        targets: '#fleche-gauche-velo',
+        scale: 1
+      });
+    });
+  
+    /*====================Bouton du bas ===========================*/
+  
+    d3.select('#ok-button').on('click', function () {
+      anime({
+        targets: 'ok-button',
+        scale: 0
+      });
+      mySlidr.slide('page-2');
+      initSlide2();
+    });
+    /*
+    d3.select('#ok-button').on('mouseover', function () {
+      anime({
+        targets: '#ok-button',
+        scale: 1.2
+      });
+  
+    });
+  
+    d3.select('#ok-button').on('mouseout', function () {
+      anime({
+        targets: '#ok-button',
+        scale: 1
+      });
+    });
+    */
+    d3.select('#plus-info-velo').on('click', function () {
+      anime({
+        targets: '#plus-info-velo',
+        scale: 0
+      });
+      mySlidr.slide('info-choix-velo');
+      initSlideInfosVelo();
+    });
+    /*
+    d3.select('#plus-info').on('mouseover', function () {
+      anime({
+        targets: '#plus-info',
+        scale: 1.2
+      });
+  
+    });
+  
+    d3.select('#plus-info').on('mouseout', function () {
+      anime({
+        targets: '#plus-info',
+        scale: 1
+      });
+    });
+    */
+   
+  
+  
+  };
 
   okbutton(infoChoix);
   infobutton();
