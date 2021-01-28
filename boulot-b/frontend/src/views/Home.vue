7c69b8f940a647963c85bd77751f71e659e121bc
Clement_Hue <clementhue@hotmail.fr>
2021-01-28 20:04:52 +0100

    ajout canari qui arrive

--- boulot-b/frontend/src/views/Home.vue
+++ boulot-b/frontend/src/views/Home.vue
@@ -1,22 +1,47 @@
 <template>
-  <div id="wazo-anim"/>
+  <div>
+    <div v-show="showAnim.logo" id="wazo-anim"/>
+    <div v-show="showAnim.canari" id="canari-come"/>
+  </div>
 </template>
 
 <script>
 import lottie from "lottie-web"
-import introAnim from "@/assets/animationJson/wazo_logo_anim.json"
+import logoWazzo from "@/assets/animationJson/wazo_logo_anim.json"
+import canariCome from "@/assets/animationJson/wazo_intro.json"
+
 export default {
   name: "Home",
+  data() {
+    return {
+      showAnim: {
+        logo: true,
+        canari: false
+      }
+    }
+  },
   mounted () {
-    const intro = lottie.loadAnimation({
+    const logo = lottie.loadAnimation({
       container : document.getElementById('wazo-anim'),
       renderer: 'svg',
       loop: false,
       autoplay: true,
-      animationData: introAnim
+      animationData: logoWazzo
     })
-    intro.addEventListener("complete", () => {
-      this.$router.push({name:"choix-type"})
+    const canari = lottie.loadAnimation({
+      container : document.getElementById('canari-come'),
+      renderer: 'svg',
+      loop: false,
+      autoplay: false,
+      animationData: canariCome
+    })
+    logo.addEventListener("complete", () => {
+      this.showAnim.logo = false;
+      this.showAnim.canari = true;
+      canari.play()
+    });
+    canari.addEventListener("complete", () => {
+      this.$router.push({name: "choix-type" })
     });
   }
 }
@@ -24,7 +49,7 @@ export default {
 
 <style scoped>
 
-#wazo-anim {
+#wazo-anim, #canari-come {
   margin-left: auto;
   margin-right: auto;
   width: 50%;
