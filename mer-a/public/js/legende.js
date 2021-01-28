8b678fe357ebfcaea39f2e4ac963fdc633ee9916
Titouan GUYADER <titouan.guyader@free.fr>
2021-01-28 21:24:39 +0100

    page finale v1

--- mer-a/public/js/legende.js
+++ mer-a/public/js/legende.js
@@ -1,20 +1,22 @@
 'use strict';
 
 (() => {
+	const scene = document.querySelector('#scene');
+	const parallaxInstance = new Parallax(scene);
 	const data = {
-		nom: 'La Baie des Trépassés', 
-    departement: 'Finistère', 
-    categorie: 'Histoires Maritimes', 
-    resume: 'Une plage macabre qui effraie les marrins de toute la Bretagne ...', 
+		nom: 'La Baie des Trépassés',
+    departement: 'Finistère',
+    categorie: 'Histoires Maritimes',
+    resume: 'Une plage macabre qui effraie les marrins de toute la Bretagne ...',
     histoire: `En breton on l’appelle Boë an Anao, qui signifie : la « baie des âmes en peine ». On raconte que les corps des marins qui avaient perdu la vie en mer venaient s’y échouer. Durant la nuit de Noël la baie résonne des chants des âmes en peine ballotées sur le bateau des morts. Une autre légende rapporte qu’elle servait de lieu d’embarquement des dépouilles des druides morts vers l’Île de Sein pour leur inhumation.
     En breton on l’appelle Boë an Anao, qui signifie : la « baie des âmes en peine ». On raconte que les corps des marins qui avaient perdu la vie en mer venaient s’y échouer. Durant la nuit de Noël la baie résonne des chants des âmes en peine ballotées sur le bateau des morts. Une autre légende rapporte qu’elle servait de lieu d’embarquement des dépouilles des druides morts vers l’Île de Sein pour leur inhumation.`,
 
-    latitude: '48.04730', 
+    latitude: '48.04730',
     longitude: '-4.70676',
     adresse: 'Cléden-Cap-Sizun 29770',
     baignade: true,
-    toilettes: false, 
-    restaurant: true, 
+    toilettes: false,
+    restaurant: true,
     photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Baie_des_Tr%C3%A9pass%C3%A9s.jpg/1920px-Baie_des_Tr%C3%A9pass%C3%A9s.jpg'
 	};
 
@@ -38,4 +40,7 @@
 		logo.src = (boolean) ? `../assets/img/logo/logo_${id}.png` : `../assets/img/logo/logo_${id}_crossed.png`;
 		logo.title = (boolean) ? alt : `pas de ${alt}`;
 	}
+	$('#myModal').on('shown.bs.modal', function () {
+	  $('#myInput').trigger('focus')
+	})
 })();
