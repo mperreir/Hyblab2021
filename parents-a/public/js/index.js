891ca1a66cb270c4aff49f4156d88b6a6e6a36ce
Baba <thedebugger.dev@gmail.com>
2021-01-29 10:02:11 +0100

    ajout transition vers page4

--- parents-a/public/js/index.js
+++ parents-a/public/js/index.js
@@ -1,5 +1,5 @@
 // init du slider (qui peut aussi faire des fondus enchainé)
-let mySlidr = slidr.create('slidr',{
+let mySlidr = slidr.create('slidr', {
     breadcrumbs: true,
     controls: 'none',
     direction: 'horizontal',
@@ -11,56 +11,56 @@ let mySlidr = slidr.create('slidr',{
     timing: { 'fade': '0.7s ease-in' },
     touch: true,
     transition: 'fade'
-  }).start();
+}).start();
 
 
 //Transition quand appuie sur logo page 1
 let initSlide1 = function() {
-  d3.select('#logo').on('click', function(){
-      mySlidr.slide('page-1_1');
-      initSlide1_1();
+    d3.select('#logo').on('click', function() {
+        mySlidr.slide('page-1_1');
+        initSlide1_1();
     });
 }
 
 //Slide de transi 
-let initSlide1_1= function() {
-    //Transi 1.2 vers 2(Camille) 
-    d3.select('#t').on('click', function(){
-        mySlidr.slide('page-2');
-        initSlide2();
-    });
-}
-/*
-let initSlide2 = function() {
-    setTimeout(suiteTraitement, 1000)
-    function suiteTraitement()
-    {
-        mySlidr.slide('page-2.1');
-        initSlide2_1();
+let initSlide1_1 = function() {
+        //Transi 1.2 vers 2(Camille) 
+        d3.select('#t').on('click', function() {
+            mySlidr.slide('page-2');
+            initSlide2();
+        });
     }
-}*/
+    /*
+    let initSlide2 = function() {
+        setTimeout(suiteTraitement, 1000)
+        function suiteTraitement()
+        {
+            mySlidr.slide('page-2.1');
+            initSlide2_1();
+        }
+    }*/
 
-function diparaitre(id){
+function diparaitre(id) {
     d3.select(id)
-    .transition()
-    .delay(20)
-    .duration(700)
-    .style("opacity", 0)
-    .style("cursor","classic")
+        .transition()
+        .delay(20)
+        .duration(700)
+        .style("opacity", 0)
+        .style("cursor", "classic")
 }
 
 //Camille se présente
 let initSlide2 = function() {
     //Transi Camille to Pret a démarrer 
-    d3.select('#button-p2-1').on('click', function(){
+    d3.select('#button-p2-1').on('click', function() {
         console.log("C'est okay")
-        console.log( d3.select('#texte-p2-1'))
-        
+        console.log(d3.select('#texte-p2-1'))
+
         //Disparition de la 1 bulle 
         diparaitre('#texte-p2-1')
         diparaitre('#vector-p2-1')
         diparaitre('#button-p2-1')
-           
+
         mySlidr.slide('page-3')
         initSlide3()
 
@@ -69,24 +69,45 @@ let initSlide2 = function() {
 
 
 //Premiere question : aventurier ? 
-let initSlide3= function() {
+let initSlide3 = function() {
     //Aventurier -> Plein la vue
-    d3.select('#bouton_non_aventurier-p3').on('click', function(){
+    d3.select('#bouton_non_aventurier-p3').on('click', function() {
         mySlidr.slide('page-4');
         initSlide4();
     });
 }
 
 //Plein la vue 
-let initSlide4= function() {
+let initSlide4 = function() {
     //Plein la vue -> avec quoi 
-    d3.select("h1").on('click', function(){
+    d3.select("#bouton_pleinLaVue-p4").on('click', function() {
         mySlidr.slide('page-5');
-        initSlide5();
+        //initSlide5();
     });
 }
 
+/*
+let initSlide5 = function() {
+    //Plein la vue -> avec quoi 
+    d3.select("#button_oui-p5").on('click', function() {
+        mySlidr.slide('page-6');
+        initSlide6();
+    });
+}
 
+let initSlide6 = function() {
+    //Plein la vue -> avec quoi 
+    d3.select("#bouton_oh_oui-p6").on('click', function() {
+        mySlidr.slide('page-7');
+        initSlide7();
+    });
+}
+
+let initSlide7 = function() {
+    console.log('arrived page 7 ');
+};
+
+*/
 
 //Initialisation du diaporama
 initSlide1();
