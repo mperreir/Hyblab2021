84e8a0c270fbdee93946846289f79d57640e9e04
Corentin Girard <corentingirard.dev@gmail.com>
2021-01-29 20:34:25 +0100

    Début boutons yes/no

--- velo-b/public/js/selectionChoix.js
+++ velo-b/public/js/selectionChoix.js
@@ -1,35 +1,29 @@
 registerSlide("infos-selection-voiture", function () {
-  const idYesBut = 'ic-oui-voit';
-  const idNoBut = 'ic-non-voit';
-  // button(idYesBut,'');
+    const idYesBut = '#ic-oui-voit';
+    const idNoBut = '#ic-non-voit';
+    button(idYesBut, 'info-choix-velo'); // TODO: changer ça
 });
 
 registerSlide("infos-selection-velo", function () {
-    const idYesBut = 'ic-oui-velo';
-    const idNoBut = 'ic-non-velo';
+    const idYesBut = '#ic-oui-velo';
+    const idNoBut = '#ic-non-velo';
 });
 
 registerSlide("infos-selection-bicloo", function () {
-    const idYesBut = 'ic-oui-bicloo';
-    const idNoBut = 'ic-non-bicloo';
+    const idYesBut = '#ic-oui-bicloo';
+    const idNoBut = '#ic-non-bicloo';
 });
 
 registerSlide("infos-selection-transport", function () {
 
 });
 
-// let initSlideSelectionTransport = function (){
-//   const idYesBut ='ic-oui-transport';
-//   const idNoBut = 'ic-non-transport';
-//
-// }
-
-// let button = function (idbutton,page) {
-//     d3.select(idbutton).on('click', function () {
-//       overrideAnim({
-//         targets: idbutton,
-//         scale: 0
-//       });
-//       goToSlide(page);
-//     });
-// }
+let button = function (idbutton, page) {
+    d3.select(idbutton).on('click', function () {
+        overrideAnim({
+            targets: idbutton,
+            scale: 0
+        });
+        goToSlide(page);
+    });
+};
