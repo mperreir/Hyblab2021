4d311d1d5500d61d9ecb77cf5b915dbcbcc3be63
Your Name <you@example.com>
2021-01-29 11:53:43 +0100

    Commentaires + Modifs BD

--- mer-a/server/generate_database.js
+++ mer-a/server/generate_database.js
@@ -1,3 +1,10 @@
+/**
+ * The file used to generate the database from the CSV.
+ */
+
+/**
+ * Imports/Constants definition
+ */
 const sqlite3 = require('sqlite3').verbose();
 const { open } = require('sqlite');
 
@@ -8,47 +15,78 @@ const config = require('./config.js');
 
 const verbose = true;
 
+/**
+ * Variables definition
+ */
 let db = null;
+//var countIdDep = 1;
+var countIdCat = 1;
+var countIdLeg = 1;
+var depList = [];
+var catList = [];
+var personnages = { data: [{cat: 'Créatures Fantastiques',
+                            //nom: 'Armelle',
+                            phrasePerso: 'Les créatures et leurs contes fantastiques',
+                            phraseDep: 'Bonjour à vous cher voyageur, cliquez sur l’un des points pour découvrir la légende qui y est associée.'},
+                          {cat: 'Croyances Religion',
+                            //nom: 'Saint-Paul',
+                            phrasePerso: 'Le moine et ses légendes religieuses',
+                            phraseDep: 'Chaque lieu renferme une légende, clique sur l’un des points pour faire ton choix mon enfant.'},
+                          {cat: 'Histoires Maritimes',
+                            //nom: 'Gwenaël',
+                            phrasePerso: 'Le marin et ses histoires maritimes',
+                            phraseDep: 'Place ton curseur sur un des points pour découvrir sa légende matelot !'}],
+getPerso: (cat) => {
+  for(perso of personnages.data) {
+    if(perso.cat === cat) return perso;
+  }
+}};
+
+/**
+ * The main function in ASYNC.
+ */
 (async () => {
   // open database
   db = await open({filename: config.ROOT + config.DB_PATH, driver: sqlite3.Database});
 
+  // SQL query to create the Departement table
   var sqlDepartement = `CREATE TABLE IF NOT EXISTS Departement (
     id INT PRIMARY KEY,
     nomDepartement VARCHAR(50) NOT NULL
-);`;
+  );`;
 
-var sqlCategorie = `CREATE TABLE IF NOT EXISTS Categorie (
+  // SQL query to create the Categorie table
+  var sqlCategorie = `CREATE TABLE IF NOT EXISTS Categorie (
     id INT PRIMARY KEY,
     nomCategorie VARCHAR(30) NOT NULL,
-    nomPersonnage VARCHAR(50) NOT NULL,
     phrasePerso VARCHAR(250) NOT NULL,
     phraseDep VARCHAR(250) NOT NULL,
     imageURI VARCHAR(10)
-);`;
+  );`;
+  // nomPersonnage VARCHAR(50) NOT NULL,
 
-  // query to create DB if not created
+  // SQL query to create the Legende table
   var sqlLegende = `CREATE TABLE IF NOT EXISTS Legende (
-              id INT PRIMARY KEY,
-              nom VARCHAR(150) NOT NULL,
-              departementId INT NOT NULL,
-              categorieId INT NOT NULL,
-              resume VARCHAR(250) NOT NULL,
-              histoire VARCHAR(1000) NOT NULL,
-              latitude DOUBLE NOT NULL,
-              longitude DOUBLE NOT NULL,
-              adresse VARCHAR(200) NOT NULL,
-              baignade BIT NOT NULL DEFAULT 0,
-              toilettes BIT NOT NULL DEFAULT 0,
-              restaurant BIT NOT NULL DEFAULT 0,
-              photo VARCHAR(500),
-              FOREIGN KEY (departementId) REFERENCES Departement(id),
-              FOREIGN KEY (categorieId) REFERENCES Categorie(id)
-          );`;
+    id INT PRIMARY KEY,
+    nom VARCHAR(150) NOT NULL,
+    departementId INT NOT NULL,
+    categorieId INT NOT NULL,
+    resume VARCHAR(250) NOT NULL,
+    histoire VARCHAR(1000) NOT NULL,
+    latitude DOUBLE NOT NULL,
+    longitude DOUBLE NOT NULL,
+    adresse VARCHAR(200) NOT NULL,
+    baignade BIT NOT NULL DEFAULT 0,
+    toilettes BIT NOT NULL DEFAULT 0,
+    restaurant BIT NOT NULL DEFAULT 0,
+    photo VARCHAR(500),
+    FOREIGN KEY (departementId) REFERENCES Departement(id),
+    FOREIGN KEY (categorieId) REFERENCES Categorie(id)
+  );`;
 
   // If necessary
   // db.run('DROP TABLE IF EXISTS Departement;');
-  db.run('DROP TABLE IF EXISTS Categorie;');
+  // db.run('DROP TABLE IF EXISTS Categorie;');
   // db.run('DROP TABLE IF EXISTS Legende;');
 
   // Execute query
@@ -61,28 +99,6 @@ var sqlCategorie = `CREATE TABLE IF NOT EXISTS Categorie (
   db.run("DELETE FROM Categorie;");
   db.run("DELETE FROM Legende;");
 
-  //var countIdDep = 1;
-  var countIdCat = 1;
-  var countIdLeg = 1;
-  var depList = [];
-  var catList = [];
-  var personnages = { data: [{cat: 'Créatures Fantastiques',
-                              nom: 'Armelle',
-                              phrasePerso: 'La fée Armelle\net ses contes fantastiques',
-                              phraseDep: 'Bonjour à vous cher voyageur, cliquez sur l’un des points pour découvrir la légende qui y est associée.'},
-                            {cat: 'Croyances Religion',
-                              nom: 'Saint-Paul',
-                              phrasePerso: 'Le moine Saint-Paul\net ses légendes religieuses',
-                              phraseDep: 'Chaque lieu renferme une légende, clique sur l’un des points pour faire ton choix mon enfant.'},
-                            {cat: 'Histoires Maritimes',
-                              nom: 'Gwenaël',
-                              phrasePerso: 'Le marin Gwenaël\net ses histoires maritimes',
-                              phraseDep: 'Place ton curseur sur un des points pour découvrir sa légende matelot !'}],
-  getPerso: (cat) => {
-    for(perso of personnages.data) {
-      if(perso.cat === cat) return perso;
-    }
-  }};
   // Fill the DB with the CSV content
   fs.createReadStream(config.ROOT + 'server/data.csv')
     .pipe(csv())
@@ -104,7 +120,7 @@ var sqlCategorie = `CREATE TABLE IF NOT EXISTS Categorie (
           '${encodeURI(catPerso.nom)}',
           '${encodeURI(catPerso.phrasePerso)}',
           '${encodeURI(catPerso.phraseDep)}',
-          'assets/img/personnage/image_${row.categorie.replace(" ", "_")}.png');\n`);
+          '${encodeURI('assets/img/personnage/' + row.categorie + '.png')}');\n`);
         catList.push(row.categorie);
         countIdCat++;
       }
