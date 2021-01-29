f6ff598a1dd542d605b9b24e1f2a462a08bdf07a
Armand BOUTTIER <armand.bouttier@etu.univ-nantes.fr>
2021-01-29 11:58:53 +0100

    nettoyage plus instructions choix departement

--- mer-a/public/js/loadmap.js
+++ mer-a/public/js/loadmap.js
@@ -5,11 +5,11 @@ function generateMap(mapData, mapFusion){
 	var width = window.innerWidth-5;
 	var height = window.innerHeight-5;
 
-	var svg = d3.select("#bretagne")
+	var svg = d3.select('#bretagne')
 		.html('')
 		.append('svg')
-		.attr("width", width)
-		.attr("height", height);
+		.attr('width', width)
+		.attr('height', height);
 
 	// Place le centre de la map
 	var center = d3.geoCentroid((() => {
@@ -21,27 +21,27 @@ function generateMap(mapData, mapFusion){
 				i--;
 			}
 		}
-		return {type: "FeatureCollection", features: useful.features};
+		return {type: 'FeatureCollection', features: useful.features};
 	})());
 
 	// Projection des longitudes et latitudes
 	var projection = d3.geoMercator()
 		.center(center)
-		.scale(width*13)
+		.scale(width*11)
 		.translate([ width /2, height/2 ])
 
 	var path = d3.geoPath().projection(projection);
 
 
 	// Dessine la map
-	svg.append("g")
-		.selectAll("path")
+	svg.append('g')
+		.selectAll('path')
 		.data(mapFusion.features)
 		.enter()
-		.append("path")
+		.append('path')
 			.attr('id',function(d) { return 'path_' + d.properties.code})
-			.attr("fill", function(d){return color(d);})
-			.attr("d", path)
+			.attr('fill', function(d){return color(d);})
+			.attr('d', path)
 			.on('mouseover', function(d){ hover(d.properties.code,this);})
 			.on('mouseleave', function(d){ leave(d.properties.code,this);})
 			.on('click', function(d){ selectDepartment(d.properties.code);})
@@ -49,17 +49,17 @@ function generateMap(mapData, mapFusion){
 			.style('stroke-width', '1px');
 
 	// Place les noms des departements
-	svg.append("g")
-		.selectAll("labels")
+	svg.append('g')
+		.selectAll('labels')
 		.data(mapFusion.features)
 		.enter()
-		.append("text")
+		.append('text')
 			.attr('id', function(d) { return 'text_' + d.properties.code})
-			.attr("x", function(d){return path.centroid(d)[0]})
-			.attr("y", function(d){return path.centroid(d)[1]})
+			.attr('x', function(d){return path.centroid(d)[0]})
+			.attr('y', function(d){return path.centroid(d)[1]})
 			.text(function(d){ return d.properties.nom})
-			.attr("text-anchor", "middle")
-			.attr("alignment-baseline", "central")
+			.attr('text-anchor', 'middle')
+			.attr('alignment-baseline', 'central')
 			.on('mouseover', function(d){
 				let codeDep = d.properties.code;
 				console.log(codeDep);
@@ -74,9 +74,9 @@ function generateMap(mapData, mapFusion){
 			.on('click', function(d){
 				let codeDep = d.properties.code;
 				selectDepartment(codeDep);})
-			.style("font-size", 28)
-			.style("fill", "white")
-			.style("display",'none');
+			.style('font-size', 28)
+			.style('fill', 'white')
+			.style('display','none');
 
 }
 
@@ -92,10 +92,11 @@ function hover(codeDep,t){
 
 		d3.select(t)
 		.transition().duration(500)
-		.style("fill-opacity", 0.95)
+		.style('fill-opacity', 0.95)
 		.style('stroke-width', '2px')
-		.style("fill", '#73b7ba');
-		d3.select('#text_' + codeDep).style("display", 'initial');
+		.style('fill', '#73b7ba')
+		.style('cursor','pointer');
+		d3.select('#text_' + codeDep).style('display', 'initial');
 	}
 }
 
@@ -105,23 +106,18 @@ function leave(codeDep,t){
 
 		d3.select(t)
 		.transition().duration(500)
-		.style("fill-opacity", 1)
+		.style('fill-opacity', 1)
 		.style('stroke-width', '1px')
-		.style("fill", '#88cbce');
-		d3.select('#text_' + codeDep).style("display", 'none');
+		.style('fill', '#88cbce')
+		.style('cursor','initial');
+		d3.select('#text_' + codeDep).style('display', 'none');
 
 	}
 }
 
-// function checkDepartment(codeDep){
-// 	if(codeDep == 2235 || codeDep == 29 || codeDep == 56) return true;
-// 	else return false;
-// }
-
-
 function selectDepartment(codeDep){
 	let id = getID(codeDep);
-	if(id > 0) document.location.href= ROOT + "personnages/" + id; 
+	if(id > 0) document.location.href= ROOT + 'personnages/' + id; 
 }
 
 function getID(code){
@@ -138,7 +134,7 @@ let deps = {data: null, isValid: (code) => {
 	generateMap(labs,mapFusion);
 })();
 
-window.addEventListener("resize", function(e) { 
+window.addEventListener('resize', function(e) { 
 	generateMap(labs,mapFusion);
 });
 
