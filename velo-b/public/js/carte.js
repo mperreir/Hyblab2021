e35c8e11240d35fd0e0b6cb56fea190c6720c961
Corentin Girard <corentingirard.dev@gmail.com>
2021-01-29 13:12:15 +0100

    Fix glitch carte

--- velo-b/public/js/carte.js
+++ velo-b/public/js/carte.js
@@ -3,17 +3,35 @@ const initCarte = function () {
         const carte = d3.select("#carte");
         carte.node().append(data.documentElement);
 
-        const parts = carte.selectAll("g")
-            .on("mouseover", function () {
-                parts.style("opacity", .5);
-                d3.select(this).style("opacity", 1);
+        carte.selectAll("g")
+            .style("opacity", .5)
+            .on("mouseenter", function () {
+                debounce.call(this, this.id, "enter", function () {
+                    console.log("[enter] " + this.id);
+                    d3.select(this).style("opacity", 1);
+                });
             })
-            .on("mouseout", () => parts.style("opacity", 1))
+            .on("mouseleave", function () {
+                debounce.call(this, this.id, "leave", function () {
+                    console.log("[leave] " + this.id);
+                    d3.select(this).style("opacity", .5);
+                });
+            })
             .on("click", function () {
                 console.log(this.id); // selectionné
-
                 mySlidr.slide('choix-transport-2');
                 initSlideChoixVelo();
             });
     });
 };
+
+let lastId, lastEvent, lastFn, lastThis;
+
+function debounce(id, event, fn) {
+    if (event === "enter" && lastId !== id) {
+        lastFn && lastFn.call(lastThis);
+        fn.call(this);
+    }
+
+    [lastId, lastEvent, lastFn, lastThis] = [id, event, fn, this];
+}
