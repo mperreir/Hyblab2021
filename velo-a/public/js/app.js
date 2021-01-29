1dd99a09be3bcfd5e533d1d8938ec9dbb4b61e62
etienne912 <etienne9912@gmail.com>
2021-01-29 09:39:23 +0100

    Fixed onclick on markers

--- velo-a/public/js/app.js
+++ velo-a/public/js/app.js
@@ -3,77 +3,90 @@ import { getStationsVelos } from "./modules/stationsVelos.mjs";
 
 async function bootstrap() {
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZGpvdmFubmlmb3VpbiIsImEiOiJja2szdGpvMHQxZW1sMm9vNWp0eHJ6ZXR1In0.KJzAGbwYjUS20dFd37YZgw';
-    const map = new mapboxgl.Map({
-        container: 'map', // container id
-        style: 'mapbox://styles/djovannifouin/ckk45pdua52v317qwdq0ijclv', // style URL
-        center: [-1.5512347469335737, 47.21611304880233], // starting position [lng, lat]
-        zoom: 11 // starting zoom
-    });
+	mapboxgl.accessToken = 'pk.eyJ1IjoiZGpvdmFubmlmb3VpbiIsImEiOiJja2szdGpvMHQxZW1sMm9vNWp0eHJ6ZXR1In0.KJzAGbwYjUS20dFd37YZgw';
+	const map = new mapboxgl.Map({
+		container: 'map', // container id
+		style: 'mapbox://styles/djovannifouin/ckk45pdua52v317qwdq0ijclv', // style URL
+		center: [-1.5512347469335737, 47.21611304880233], // starting position [lng, lat]
+		zoom: 11 // starting zoom
+	});
 
-    // Départ et arrivée: https://github.com/mapbox/mapbox-gl-directions/blob/master/API.md
-    let control = new MapboxDirections({
-        accessToken: mapboxgl.accessToken,
-        unit: 'metric',
-        profile: 'mapbox/cycling',
-        language: 'fr',
-        alternatives: true,
-        placeholderOrigin: 'Départ',
-        placeholderDestination: 'Arrivée',
-        controls: {
-            profileSwitcher: false,
-            instructions: false
-        }
-    });
+	// Départ et arrivée: https://github.com/mapbox/mapbox-gl-directions/blob/master/API.md
+	let control = new MapboxDirections({
+		accessToken: mapboxgl.accessToken,
+		unit: 'metric',
+		profile: 'mapbox/cycling',
+		language: 'fr',
+		alternatives: true,
+		placeholderOrigin: 'Départ',
+		placeholderDestination: 'Arrivée',
+		controls: {
+			profileSwitcher: false,
+			instructions: false
+		}
+	});
 
-    document.getElementById('mapbox-controllers').appendChild(control.onAdd(map))
+	document.getElementById('mapbox-controllers').appendChild(control.onAdd(map))
 
-    map.addControl(
-        new mapboxgl.GeolocateControl({
-            positionOptions: {
-                enableHighAccuracy: true
-            },
-            trackUserLocation: true
-        })
-    )
+	map.addControl(
+		new mapboxgl.GeolocateControl({
+			positionOptions: {
+				enableHighAccuracy: true
+			},
+			trackUserLocation: true
+		})
+	)
 
-    function points(data, url) {
+	let openMarker = undefined;
 
-        data.forEach((d) => {
-            const el = document.createElement("div");
-            el.className = "marker";
-            el.style.backgroundImage = `url(${url})`;
+	function points(data, url) {
 
-            new mapboxgl.Marker(el)
-                .setLngLat([d.longitude, d.latitude])
-                .setPopup(new mapboxgl.Popup().setHTML(d.text))
-                .addTo(map);
-        });
-    }
+		data.forEach((d) => {
+			const el = document.createElement("div");
+			el.className = "marker";
+			el.style.backgroundImage = `url(${url})`;
 
-    abrisVeloDisplayData().then(data => {
-        points(data, "https://svgshare.com/i/TVr.svg");
-    });
+			let marker;
+			el.addEventListener("click", function (event) {
+				// close the holde popup (if active)
+				if (openMarker) openMarker._popup.remove();
+				// open the popup
+				marker._popup.addTo(map)
+				openMarker = marker;
+				event.stopPropagation();
+			});
 
-    getStationsVelos().then(data => {
-        points(data, "https://svgshare.com/i/TUq.svg");
-    });
+
+			marker = new mapboxgl.Marker(el)
+				.setLngLat([d.longitude, d.latitude])
+				.setPopup(new mapboxgl.Popup().setHTML(d.text))
+				.addTo(map);
+		});
+	}
+
+	abrisVeloDisplayData().then(data => {
+		points(data, "https://svgshare.com/i/TVr.svg");
+	});
+
+	getStationsVelos().then(data => {
+		points(data, "https://svgshare.com/i/TUq.svg");
+	});
 }
 
 bootstrap();
 
 document.getElementById("input-meteo").onclick = () => {
-    document.location.href = "question.html?page=météo";
+	document.location.href = "question.html?page=météo";
 };
 
 document.getElementById("input-pollution").onclick = () => {
-    document.location.href = "question.html?page=pollution";
+	document.location.href = "question.html?page=pollution";
 };
 
 document.getElementById("input-activite").onclick = () => {
-    document.location.href = "question.html?page=activité";
+	document.location.href = "question.html?page=activité";
 };
 
 document.getElementById("input-vae").onclick = () => {
-    document.location.href = "question.html?page=VAE";
+	document.location.href = "question.html?page=VAE";
 };
