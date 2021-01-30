8dfc5551978d394bbe178891f88295cccbe0ec60
Your Name <you@example.com>
2021-01-30 18:13:54 +0100

    correct loop audio

--- mer-a/public/js/script.js
+++ mer-a/public/js/script.js
@@ -7,6 +7,7 @@
 (async () => {
   mapFusion = await (await fetchAsync('/mer-a/assets/data/map.json', 'GET')).json();
   $('#vagues_sound')[0].volume = 0.3;
+  $('#vagues_sound')[0].loop = true;
 })();
 
 window.onload = () => {
