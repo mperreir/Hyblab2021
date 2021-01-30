060a8d707e84b1e4246ad78921fe2a386ea09f7f
Loïc Boursin <32396605+LoicBoursin@users.noreply.github.com>
2021-01-30 18:04:58 +0100

    Add cursor and fixes (#46)
    Amélioration de la page "Crédits"

--- parents-b/public/js/creditsPage.js
+++ parents-b/public/js/creditsPage.js
@@ -1,13 +1,17 @@
 let initCredits = function(){
 
+    d3.select('.go-back-end').on('click', function () {
+        mySlidr.slide('left');
+        initEnd();
+    });
+
     d3.select('.more-info').on('click', function (){
         //Doit ouvrir la page qui sommes nous
     });
-    
-    console.log('credits chargés');
-    
+
     d3.select('.logoAccueil9').on('click', function (){
         mySlidr.slide('home-page');
+        resetHome();
         initHome();
     });
 
