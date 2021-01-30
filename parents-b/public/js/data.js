4dba7aeecc8e94bb39568854b8f1b67640a1eeb8
Nakline <nathan.genaudeau@gmail.com>
2021-01-30 01:07:30 +0100

    Fin de la page de resultats

--- parents-b/public/js/data.js
+++ parents-b/public/js/data.js
@@ -423,23 +423,26 @@ function fetchData() {
             const address1 = document.getElementById('result-one-address');
             const address2 = document.getElementById('result-two-address');
             const address3 = document.getElementById('result-three-address');
-            address1.innerHTML += ' ' +  top[0]['Adresse'];
-            address2.innerHTML += ' ' + top[1]['Adresse'];
-            address3.innerHTML += ' ' + top[2]['Adresse'];
+            address1.innerHTML = '<img src="img/results_page/address.svg" alt="Icone d\'adresse">' + ' ' + top[0]['Adresse'];
+            address2.innerHTML = '<img src="img/results_page/address.svg" alt="Icone d\'adresse">' + ' ' + top[1]['Adresse'];
+            address3.innerHTML = '<img src="img/results_page/address.svg" alt="Icone d\'adresse">' + ' ' + top[2]['Adresse'];
 
             const access1 = document.getElementById('result-one-access');
             const access2 = document.getElementById('result-two-access');
             const access3 = document.getElementById('result-three-access');
-            access1.innerHTML += ' ' + top[0]['Accès transports en commun'];
-            access2.innerHTML += ' ' + top[1]['Accès transports en commun'];
-            access3.innerHTML += ' ' + top[2]['Accès transports en commun'];
+            access1.innerHTML = '<img src="img/results_page/tram.svg" alt="Icone d\'accès">' + ' ' + top[0]['Accès transports en commun'];
+            access2.innerHTML = '<img src="img/results_page/tram.svg" alt="Icone d\'accès">' + ' ' + top[1]['Accès transports en commun'];
+            access3.innerHTML = '<img src="img/results_page/tram.svg" alt="Icone d\'accès">' + ' ' + top[2]['Accès transports en commun'];
 
             const time1 = document.getElementById('result-one-time');
             const time2 = document.getElementById('result-two-time');
             const time3 = document.getElementById('result-three-time');
-            time1.innerHTML += ' ' + top[0]['Horaires d\'ouverture'];
-            time2.innerHTML += ' ' + top[1]['Horaires d\'ouverture'];
-            time3.innerHTML += ' ' + top[2]['Horaires d\'ouverture'];
+            let horaire1 = top[0]['Horaires d\'ouverture'] === "00h01-23h59" ? "Ouvert 24h/24h" : top[0]['Horaires d\'ouverture'];
+            let horaire2 = top[1]['Horaires d\'ouverture'] === "00h01-23h59" ? "Ouvert 24h/24h" : top[1]['Horaires d\'ouverture'];
+            let horaire3 = top[2]['Horaires d\'ouverture'] === "00h01-23h59" ? "Ouvert 24h/24h" : top[2]['Horaires d\'ouverture'];
+            time1.innerHTML = '<img src="img/results_page/time.svg" alt="Icone d\'ouverture">' + ' ' + horaire1;
+            time2.innerHTML = '<img src="img/results_page/time.svg" alt="Icone d\'ouverture">' + ' ' + horaire2;
+            time3.innerHTML = '<img src="img/results_page/time.svg" alt="Icone d\'ouverture">' + ' ' + horaire3;
 
             const location1 = document.getElementById('result-one-location');
             const location2 = document.getElementById('result-two-location');
@@ -448,19 +451,142 @@ function fetchData() {
             location2.href = 'https://www.google.fr/maps/@' + top[1]['Géolocalisation'] + ',18z';
             location3.href = 'https://www.google.fr/maps/@' + top[2]['Géolocalisation'] + ',18z';
 
-            // Affichage de la page finale
-            for (const line of top) {
-                /*
-                for (const [key, value] of Object.entries(line)) {
-                    if (myCriteria[key]) {
-                        if (myCriteria[key] != null && myCriteria[key] === value) {
-                            line["nbElemCorrect"]++;
-                        }
-                    }
+            let slider1 = document.getElementById("splide-one-inside");
+            let slider2 = document.getElementById("splide-two-inside");
+            let slider3 = document.getElementById("splide-three-inside");
+            slider1.innerHTML = '';
+            slider2.innerHTML = '';
+            slider3.innerHTML = '';
+
+            for (const [key, value] of Object.entries(top[0])) {
+                if (value === true){
+                    if (key === "Accessibilité Handicapé") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_disabled_picto.svg"></li>';
+                    if (key === "Accès Parking") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_parking_picto.svg"></li>';
+                    if (key === "Accès transports en commun") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_tram_picto.svg"></li>';
+                    if (key === "Activités organisées") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_activities_picto.svg"></li>';
+                    if (key === "Bancs") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_bench_picto.svg"></li>';
+                    if (key === "CRAPA") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_crapa_picto.svg"></li>';
+                    if (key === "Chiens autorisés") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_dog_picto.svg"></li>';
+                    if (key === "Herbe (un minimum) / Sable") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_grass_picto.svg"></li>';
+                    if (key === "Jardin clos") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_fence_picto.svg"></li>';
+                    if (key === "Jeux pour enfants") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_games_picto.svg"></li>';
+                    if (key === "Point d'eau") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_pond_picto.svg"></li>';
+                    if (key === "Présence d'animaux") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_animals_picto.svg"></li>';
+                    if (key === "Restauration") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_restaurant_picto.svg"></li>';
+                    if (key === "Sanitaires") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_wc_picto.svg"></li>';
+                    if (key === "Table pique-nique") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_table_picto.svg"></li>';
+                    if (key === "Terrains de sport") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_sport_field_picto.svg"></li>';
+                    if (key === "Verdure / Plante Espace Vert") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_tree_picto.svg"></li>';
+                    if (key === "Élément de culture") slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_statue_picto.svg"></li>';
                 }
-                */
             }
-        })
+
+            for (const [key, value] of Object.entries(top[1])) {
+                if (value === true){
+                    if (key === "Accessibilité Handicapé") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_disabled_picto.svg"></li>';
+                    if (key === "Accès Parking") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_parking_picto.svg"></li>';
+                    if (key === "Accès transports en commun") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_tram_picto.svg"></li>';
+                    if (key === "Activités organisées") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_activities_picto.svg"></li>';
+                    if (key === "Bancs") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_bench_picto.svg"></li>';
+                    if (key === "CRAPA") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_crapa_picto.svg"></li>';
+                    if (key === "Chiens autorisés") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_dog_picto.svg"></li>';
+                    if (key === "Herbe (un minimum) / Sable") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_grass_picto.svg"></li>';
+                    if (key === "Jardin clos") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_fence_picto.svg"></li>';
+                    if (key === "Jeux pour enfants") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_games_picto.svg"></li>';
+                    if (key === "Point d'eau") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_pond_picto.svg"></li>';
+                    if (key === "Présence d'animaux") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_animals_picto.svg"></li>';
+                    if (key === "Restauration") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_restaurant_picto.svg"></li>';
+                    if (key === "Sanitaires") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_wc_picto.svg"></li>';
+                    if (key === "Table pique-nique") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_table_picto.svg"></li>';
+                    if (key === "Terrains de sport") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_sport_field_picto.svg"></li>';
+                    if (key === "Verdure / Plante Espace Vert") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_tree_picto.svg"></li>';
+                    if (key === "Élément de culture") slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_statue_picto.svg"></li>';
+                }
+            }
+
+            for (const [key, value] of Object.entries(top[2])) {
+                if (value === true){
+                    if (key === "Accessibilité Handicapé") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_disabled_picto.svg"></li>';
+                    if (key === "Accès Parking") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_parking_picto.svg"></li>';
+                    if (key === "Accès transports en commun") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_tram_picto.svg"></li>';
+                    if (key === "Activités organisées") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_activities_picto.svg"></li>';
+                    if (key === "Bancs") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_bench_picto.svg"></li>';
+                    if (key === "CRAPA") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_crapa_picto.svg"></li>';
+                    if (key === "Chiens autorisés") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_dog_picto.svg"></li>';
+                    if (key === "Herbe (un minimum) / Sable") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_grass_picto.svg"></li>';
+                    if (key === "Jardin clos") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_fence_picto.svg"></li>';
+                    if (key === "Jeux pour enfants") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_games_picto.svg"></li>';
+                    if (key === "Point d'eau") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_pond_picto.svg"></li>';
+                    if (key === "Présence d'animaux") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_animals_picto.svg"></li>';
+                    if (key === "Restauration") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_restaurant_picto.svg"></li>';
+                    if (key === "Sanitaires") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_wc_picto.svg"></li>';
+                    if (key === "Table pique-nique") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_table_picto.svg"></li>';
+                    if (key === "Terrains de sport") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_sport_field_picto.svg"></li>';
+                    if (key === "Verdure / Plante Espace Vert") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_tree_picto.svg"></li>';
+                    if (key === "Élément de culture") slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_statue_picto.svg"></li>';
+                }
+            }
+
+            slider1.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_bin_picto.svg"></li>';
+            slider2.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_bin_picto.svg"></li>';
+            slider3.innerHTML += '<li class="splide__slide"><img class="icon-slider" src="img/common/pictograms/small/small_bin_picto.svg"></li>';
+
+
+
+            new Splide( '#splide-one', {
+                type : 'loop',
+                perPage: 5,
+                perMove: 5,
+                focus  : 'center',
+                heightRatio: 0.19,
+                arrows: false,
+                pagination: false,
+                autoplay: true,
+                speed: 25000,
+                rewind: false,
+                interval: 0,
+                cover: true,
+                gap: '0.3vw',
+                easing: 'linear',
+            } ).mount();
+
+            new Splide( '#splide-two', {
+                type : 'loop',
+                perPage: 5,
+                perMove: 5,
+                focus  : 'center',
+                heightRatio: 0.19,
+                arrows: false,
+                pagination: false,
+                autoplay: true,
+                speed: 25000,
+                rewind: false,
+                interval: 0,
+                cover: true,
+                gap: '0.3vw',
+                easing: 'linear',
+            } ).mount();
+
+
+            new Splide( '#splide-three', {
+                type : 'loop',
+                perPage: 5,
+                perMove: 5,
+                focus  : 'center',
+                heightRatio: 0.19,
+                arrows: false,
+                pagination: false,
+                autoplay: true,
+                speed: 25000,
+                rewind: false,
+                interval: 0,
+                cover: true,
+                gap: '0.3vw',
+                easing: 'linear',
+            } ).mount();
+
+
+        });
 }
 
 function main() {
