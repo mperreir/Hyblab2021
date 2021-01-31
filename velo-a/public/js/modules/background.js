ce9b3c3f16b7e26f2d6f665304a99424feafd888
Alban MOIZAN <alban.moizan@etu.univ-nantes.fr>
2021-01-31 23:24:01 +0100

    improved background animation

--- velo-a/public/js/modules/background.js
+++ velo-a/public/js/modules/background.js
@@ -2,6 +2,7 @@
 export function slide(onStart, onContinue, slides) {
 
     velosSlide();
+    nuagesSlide();
 
     let start = true;
     let i = 0;
@@ -14,7 +15,6 @@ export function slide(onStart, onContinue, slides) {
                 document.getElementById("batiment").className = "batiment_pause" + i;
                 setTimeout(function () {
                     document.getElementById("batiment").className = "batiment_move" + (++i);
-                    console.log(i)
                     if (i <= 0) batiment_return.style.display = "none";
                     else {
                         batiment_return.style.display = null;
@@ -71,12 +71,42 @@ function _velosSlide(velos, i) {
 
         const background_velos = document.getElementById("background_velos");
         background_velos.insertAdjacentHTML('beforeend',
-            "<img id='background_velo"+ i +"' class=\"background_velo move"+ Math.round(Math.random()) +"\" " + velos[rand] + "/>"
+            "<img id='background_velo"+ i +"' class='background_velo move"+ Math.round(Math.random()) +"' " + velos[rand] + "/>"
         );
-        const thisI = i;
         setTimeout(function () {
             background_velos.firstChild.remove();
         }, 10000);
         _velosSlide(velos, ++i);
+    }, time);
+}
+
+
+function nuagesSlide() {
+
+    _nuagesSlide([
+        'src="img/cloud1.svg" alt="Vélo classique"',
+        'src="img/cloud2.svg" alt="Vélo électrique"'
+    ], 0);
+}
+
+function _nuagesSlide(nuages, i) {
+    const time =  Math.floor(Math.random()*10000);
+    setTimeout(function () {
+        const rand = Math.round(Math.random());
+
+        const background_nuages = document.getElementById("background_nuages");
+        background_nuages.insertAdjacentHTML('beforeend',
+            "<img alt='nuage' id='background_nuage"+ i +"' "
+            +"class='background_nuage move"+ Math.round(Math.random())
+            +"' " + nuages[rand]
+            + "/>"
+        );
+
+        background_nuages.lastElementChild.style.setProperty("--nuage-top", Math.floor(Math.random()*35) +"%");
+        background_nuages.lastElementChild.style.setProperty("--nuage-size",  (Math.floor(Math.random()*100)+100) + "px");
+        setTimeout(function () {
+            background_nuages.firstChild.remove();
+        }, 35000);
+        _nuagesSlide(nuages, ++i);
     }, time);
 }
