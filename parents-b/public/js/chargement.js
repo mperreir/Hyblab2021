fc731a491ec8d8307f061a56c56debda7ad920d7
Colin RENAUD <colin.renaud5@gmail.com>
2021-01-31 13:21:07 +0100

    ajout fade chargement

--- parents-b/public/js/chargement.js
+++ parents-b/public/js/chargement.js
@@ -2,9 +2,12 @@ $(document).ready(function(){
   $("#home")[0].style.display = "none";
   $(".shooting-stars")[0].style.display = "none";
   setTimeout(function () {
-    $("#chargementI")[0].style.display = "none";
-    document.querySelector("body").className = "background-black";
-    $("#home")[0].style.display = "block";
-    // $(".shooting-stars")[0].style.display = "block";
+    $("#chargementI")[0].style.transition = "opacity 1s ease-out";
+    $("#chargementI")[0].style.opacity = 0;
+    setTimeout(function () {
+        document.querySelector("body").className = "background-black";
+        $("#home")[0].style.display = "block";
+        $("#chargementI")[0].style.display = "none";
+    }, 1000);
 }, 4000);
 });
