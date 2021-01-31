933ca73c20354cfe4dd409146bf352266d21af11
Colin RENAUD <colin.renaud5@gmail.com>
2021-01-31 13:24:06 +0100

    merge

--- parents-b/public/js/chargement.js
+++ parents-b/public/js/chargement.js
@@ -2,20 +2,13 @@ $(document).ready(function(){
   $("#home")[0].style.display = "none";
   $(".shooting-stars")[0].style.display = "none";
   setTimeout(function () {
-<<<<<<< HEAD
     $("#chargementI")[0].style.transition = "opacity 1s ease-out";
     $("#chargementI")[0].style.opacity = 0;
     setTimeout(function () {
         document.querySelector("body").className = "background-black";
         $("#home")[0].style.display = "block";
         $("#chargementI")[0].style.display = "none";
+        $(".shooting-stars")[0].style.display = "block";
     }, 1000);
 }, 4000);
-=======
-    $("#chargementI")[0].style.display = "none";
-    document.querySelector("body").className = "background-black";
-    $("#home")[0].style.display = "block";
-    $(".shooting-stars")[0].style.display = "block";
-  }, 4500);
->>>>>>> d28614cac5936c5fce3b5fb7cb82c9f0812e4149
 });
