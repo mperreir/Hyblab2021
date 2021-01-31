6a3ab02d1c6b3066d76a75bd32dd096e3fd0909a
Lucas HERVOUET <lucas.hervouet@etu.univ-nantes.fr>
2021-01-31 18:27:56 +0100

    Add Bicky on hover question button

--- velo-a/public/js/starterPack.js
+++ velo-a/public/js/starterPack.js
@@ -67,6 +67,18 @@ async function bootstrap() {
 
 bootstrap();
 
+let bickySavoir = document.getElementsByClassName("bicky-savoir");
+
+document.getElementById("button-question").onmouseover = () => {
+    bickySavoir.item(0).style.visibility = "visible";
+    bickySavoir.item(1).style.visibility = "visible";
+}
+
+document.getElementById("button-question").onmouseout = () => {
+    bickySavoir.item(0).style.visibility = "hidden";
+    bickySavoir.item(1).style.visibility = "hidden";
+}
+
 function points(varName, data, url) {
 
     data.forEach((d) => {
