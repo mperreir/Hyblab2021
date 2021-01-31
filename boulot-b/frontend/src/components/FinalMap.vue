b3425b058f7eb7a3ea7fffd01f062cde692c19af
Antoine Garnier <antoine.garnier1@etu.univ-nantes.fr>
2021-01-31 15:17:31 +0100

    Refactoring du code

--- boulot-b/frontend/src/components/FinalMap.vue
+++ boulot-b/frontend/src/components/FinalMap.vue
@@ -48,23 +48,14 @@ async function createMap(platform, map, choices) {
     // var style = new H.map.Style("../assets/map/wazo_map.yaml",
     // 'https://js.api.here.com/v3/3.1/styles/omv/');
     // provider.setStyle(style);
-
     
     const datas = await getDatas(choices);
 
     const transportType = (choices.typeDeplacement === 'pied' ? 'pedestrian' : 'bicycle');
 
-    //const coordStopsList = [['47.217371', '-1.573621']]
-    //if (this.transportType == 'velo') { transportType = 'bicycle' }
-  
-    //const coordStops = coordStopsList.map(coords => coords[0] + "," + coords[1])
-
     console.log("origin");
     console.log(origin);
     console.log(datas.data.Depart)
-
-    // const depart = ['47.218371', '-1.553621'];
-    // const arrivee = ['47.217371', '-1.593621'];
 
     await calculateRouteFromAtoB(platform, map, datas.data.Depart, datas.data.Arrivee, datas.data.POI, transportType);
 }
@@ -76,7 +67,7 @@ async function getDatas(choices) {
     // const destination = "2 Rue Saint-Stanislas, 44000 Nantes";
 
     const typeDeplacement = (choices.typeDeplacement === "velo" ? "bicycle" : "pedestrian");
-    //const style = (choices.style === "boulevards" ? "boullevards" : "ruelles");
+
     let theme = "alea";
     switch (choices.theme) {
         case "nature":
@@ -171,54 +162,45 @@ async function addRouteShapeToMap(route, map, origin, destination, stops){
     //   bounds: polyline.getBoundingBox()
     // });
   });
-  const point = await createImage('point.png')
-  const iconPoint = new H.map.DomIcon(point);
 
-  const imageOrigin = await createImage('origin.png');
-  const iconOrigin = new H.map.DomIcon(imageOrigin);
+  await addMarkers(map, origin, destination, stops);
+}
 
-  const imageDestination = await createImage('destination.png');
-  const iconDestination = new H.map.DomIcon(imageDestination);
+async function addMarkers(map, origin, destination, stops) {
+  const iconPoint = await createIcon('point.png');
+  const iconOrigin = await createIcon('origin.png');
 
-  const coordsOrigin = {lat: origin[0], lng: origin[1]};
-  const coordsDestination = {lat: destination[0], lng: destination[1]};
+  const markerOrigin = new H.map.DomMarker({lat: origin[0], lng: origin[1]}, {icon: iconOrigin});
+  const markerDestination = new H.map.DomMarker({lat: destination[0], lng: destination[1]}, {icon: iconDestination});
+  
+  map.addObject(markerOrigin);
+  map.addObject(markerDestination);
+
+  const iconDestination = await createIcon('destination.png');
+
   const coordStops = stops.map((object) => {
     return [Object.values(object)[0].coordonnees['lat'], Object.values(object)[0].coordonnees['lng']]
   });
 
   console.log(coordStops[0])
 
-  const markerOrigin = new H.map.DomMarker(coordsOrigin, {icon: iconOrigin});
-  const markerDestination = new H.map.DomMarker(coordsDestination, {icon: iconDestination});
-  
-  map.addObject(markerOrigin);
-  map.addObject(markerDestination);
-
   for (let i=0 ; i < coordStops.length; i++) {
     const markerStop = new H.map.DomMarker({lat: coordStops[i][0], lng: coordStops[i][1]}, {icon: iconPoint});
     map.addObject(markerStop);
   }
-  
-  
+}
 
-  async function createImage(imageName) {
+async function createIcon(imageName) {
     console.log('Requête image')
     console.log('/boulot-b/getUrlImage/' + imageName)
     const reqUrlImage = await axios.get(`/boulot-b/getUrlImage/${imageName}`);
-    const imagePoint = document.createElement('img');
+    const image = document.createElement('img');
     //console.log(reqUrlImage.data.urlImage);
-    imagePoint.src = reqUrlImage.data.urlImage;
-    imagePoint.width = 10;
-    imagePoint.height = 10;
-    return imagePoint;
+    image.src = reqUrlImage.data.urlImage;
+    image.width = 10;
+    image.height = 10;
+    return new H.map.DomIcon(image);
   }
-
-  // async function showTrajet() {
-  //   console.log('début promesse et requete get')
-  //   const res = await axios.get('/boulot-b/trajet/3+rue+christian+pauc+nantes/7+rue+george+berthome+nantes/pedestrian/nature/false/false/true/false')
-  //   console.log(res)
-  // }
-}
 
 </script>
 
