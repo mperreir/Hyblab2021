d94c1f886f965a4dcd035771996b3158a6bef38f
Corentin Girard <corentingirard.dev@gmail.com>
2021-02-01 15:21:38 +0100

    Fix progressbar colors

--- velo-b/public/js/progressBar.js
+++ velo-b/public/js/progressBar.js
@@ -4,5 +4,5 @@ function progressBar(idProgressBar, value) {
 
     d3.select(idProgressBar)
         .attr("class", "progressBar")
-        .html(`<svg width="353" height="38"><rect x="8" y="8" rx="11" ry="11" width="${(value / 1) * 345}" height="22" style="fill: #f98e12"/></svg>`);
+        .html(`<svg width="353" height="38"><rect x="8" y="8" rx="11" ry="11" width="${(value / 1) * 345}" height="22"/></svg>`);
 }
