3066c4579afbffe514d8586be6d24c4e608dada0
Alban MOIZAN <alban.moizan@etu.univ-nantes.fr>
2021-02-01 19:25:23 +0100

    modified home animation and added a button to pass it

--- velo-a/public/js/index.js
+++ velo-a/public/js/index.js
@@ -1,9 +1,28 @@
 "use strict";
 
 async function bootstrap() {
-	setTimeout(function () {
-		document.location = 'information.html';
-	}, 37000);
+	const animation = bodymovin.loadAnimation({
+		container: document.getElementById('lottie'), // Required
+		path: 'resources/home.json', // Required
+		renderer: 'svg', // Required
+		loop: false, // Optional
+		autoplay: false, // Optional
+		name: "home page", // Name for future reference. Optional.
+		rendererSettings: {
+			preserveAspectRatio: "xMaxYMax meet"
+		}
+	})
+
+	console.log('ici1')
+	animation.addEventListener("DOMLoaded", () => {
+		console.log('ici2')
+		animation.play();
+
+		animation.addEventListener('complete', () => {
+			document.location = 'information.html';
+		});
+
+	})
 }
 
 window.addEventListener('DOMContentLoaded', () => {
--- velo-a/public/js/modules/background.js
+++ velo-a/public/js/modules/background.js
@@ -1,114 +1,111 @@
 export function slide(onStart, onContinue, slides) {
 
-	velosSlide();
-	nuagesSlide();
+    velosSlide();
+    nuagesSlide();
 
-	let start = true;
-	let i = 0;
-	const batiment_return = document.getElementById("batiment_return");
+    let start = true;
+    let i = 0;
+    const batiment_return = document.getElementById("batiment_return");
 
-	if(batiment_return) batiment_return.style.display = "none";
-	document.querySelectorAll(".batiment_button").forEach((el) => {
-		if (slides == null || slides[slides.length - 1] !== el.parentElement.id)
-			el.addEventListener('click', () => {
-				document.getElementById("batiment").className = "batiment_pause" + i;
-				setTimeout(function () {
-					document.getElementById("batiment").className = "batiment_move" + (++i);
-					if(batiment_return) {
-						if (i <= 0) batiment_return.style.display = "none";
-						else {
-							batiment_return.style.display = null;
-							if (start) {
-								onStart();
-								start = false;
-							}
-						}
-					}
-				}, 50);
+    batiment_return.style.display = "none";
+    document.querySelectorAll(".batiment_button").forEach((el) => {
+        if(slides == null || slides[slides.length-1] !== el.parentElement.id)
+            el.addEventListener('click', () => {
+                document.getElementById("batiment").className = "batiment_pause" + i;
+                setTimeout(function () {
+                    document.getElementById("batiment").className = "batiment_move" + (++i);
+                    if (i <= 0) batiment_return.style.display = "none";
+                    else {
+                        batiment_return.style.display = null;
+                        if (start) {
+                            onStart();
+                            start = false;
+                        }
+                    }
+                }, 50);
 
-			});
-	});
-	if(batiment_return) {
-		batiment_return.addEventListener('click', () => {
-			document.getElementById("batiment").className = "batiment_pause" + i;
-			setTimeout(function () {
-				document.getElementById("batiment").className = "batiment_back" + (i--);
-				if (i <= 0) {
-					batiment_return.style.display = "none";
-				}
+            });
+    });
 
-				const el = document.querySelector(".show > .batiment_button");
+    batiment_return.addEventListener('click', () => {
+        document.getElementById("batiment").className = "batiment_pause" + i;
+        setTimeout(function () {
+            document.getElementById("batiment").className = "batiment_back" + (i--);
+            if (i <= 0) {
+                batiment_return.style.display = "none";
+            }
 
-				onContinue(el, slides, false);
+            const el = document.querySelector(".show > .batiment_button");
 
-			}, 50);
-		});
-	}
+            onContinue(el, slides, false);
 
-	document.querySelectorAll(".batiment_button").forEach((el, index, list) => {
-		if (slides == null || el.parentElement.parentElement.id !== slides[0]) {
-			el.parentElement.setAttribute("class", "hide_start");
-		}
-		if (slides == null || slides[slides.length - 1] !== el.parentElement.id)
-			el.addEventListener("click", () => {
-				onContinue(el, slides, true);
-			});
-	});
+        }, 50);
+    });
+
+    document.querySelectorAll(".batiment_button").forEach((el, index, list) => {
+        if (slides == null || el.parentElement.parentElement.id !== slides[0]) {
+            el.parentElement.setAttribute("class", "hide_start");
+        }
+        if(slides == null || slides[slides.length-1] !== el.parentElement.id)
+            el.addEventListener("click", () => {
+                onContinue(el, slides, true);
+            });
+    });
 
 }
 
 function velosSlide() {
 
-	_velosSlide([
-		"src=\"img/velos/simple.svg\" alt=\"Vélo classique\"",
-		"src=\"img/velos/elec.svg\" alt=\"Vélo électrique\"",
-		"src=\"img/velos/bicloo.svg\" alt=\"Le bicloo\""
-	], 0);
+    _velosSlide([
+        "src=\"img/velos/simple.svg\" alt=\"Vélo classique\"",
+        "src=\"img/velos/elec.svg\" alt=\"Vélo électrique\"",
+        "src=\"img/velos/bicloo.svg\" alt=\"Le bicloo\""
+    ], 0);
 }
 
 function _velosSlide(velos, i) {
-	const time = Math.floor(Math.random() * 5000);
-	setTimeout(function () {
-		const rand = Math.round(Math.random() * 2);
+    const time =  Math.floor(Math.random()*10000);
+    setTimeout(function () {
+        const rand = Math.round(Math.random()*2);
 
-		const background_velos = document.getElementById("background_velos");
-		background_velos.insertAdjacentHTML('beforeend',
-			"<img id='background_velo" + i + "' class='background_velo move" + Math.round(Math.random()) + "' " + velos[rand] + "/>"
-		);
-		setTimeout(function () {
-			background_velos.firstChild.remove();
-		}, 10000);
-		_velosSlide(velos, ++i);
-	}, time);
+        const background_velos = document.getElementById("background_velos");
+        background_velos.insertAdjacentHTML('beforeend',
+            "<img id='background_velo"+ i +"' class='background_velo move"+ Math.round(Math.random()) +"' " + velos[rand] + "/>"
+        );
+        setTimeout(function () {
+            background_velos.firstChild.remove();
+        }, 10000);
+        _velosSlide(velos, ++i);
+    }, time);
 }
 
 
 function nuagesSlide() {
 
-	_nuagesSlide([
-		'src="img/background/cloud1.svg" alt="Vélo classique"',
-		'src="img/background/cloud2.svg" alt="Vélo électrique"'
-	], 0);
+    _nuagesSlide([
+        'src="img/background/cloud1.svg" alt="Vélo classique"',
+        'src="img/background/cloud2.svg" alt="Vélo électrique"'
+    ], 0);
 }
 
 function _nuagesSlide(nuages, i) {
-	const time = Math.floor(Math.random() * 10000);
-	setTimeout(function () {
-		const rand = Math.round(Math.random());
+    const time =  Math.floor(Math.random()*20000);
+    setTimeout(function () {
+        const rand = Math.round(Math.random());
 
-		const background_nuages = document.getElementById("background_nuages");
-		background_nuages.insertAdjacentHTML('beforeend',
-			"<img alt='nuage' id='background_nuage" + i + "' "
-			+ "class='background_nuage move" + Math.round(Math.random())
-			+ "' " + nuages[rand]
-			+ "/>"
-		);
+        const background_nuages = document.getElementById("background_nuages");
+        background_nuages.insertAdjacentHTML('beforeend',
+            "<img alt='nuage' id='background_nuage"+ i +"' "
+            +"class='background_nuage move"+ Math.round(Math.random())
+            +"' " + nuages[rand]
+            + "/>"
+        );
 
-		background_nuages.lastElementChild.style.setProperty("--nuage-top", Math.floor(Math.random() * 35) + "%");
-		background_nuages.lastElementChild.style.setProperty("--nuage-size", (Math.floor(Math.random() * 100) + 100) + "px");
-		setTimeout(function () {
-			background_nuages.firstChild.remove();
-		}, 35000);
-		_nuagesSlide(nuages, ++i);
-	}, time);
+        background_nuages.lastElementChild.style.setProperty("--nuage-top", Math.floor(Math.random()*35) +"%");
+        background_nuages.lastElementChild.style.setProperty("--nuage-size",  (Math.floor(Math.random()*100)+100) + "px");
+        setTimeout(function () {
+            background_nuages.firstChild.remove();
+        }, 35000);
+        _nuagesSlide(nuages, ++i);
+    }, time);
 }
