58f338cac081e1430c66acca83019b51ef1ba820
Your Name <you@example.com>
2021-02-01 12:31:49 +0100

    suppression de config.js + generalisation ROOT

--- mer-a/public/js/common/router.js
+++ mer-a/public/js/common/router.js
@@ -2,6 +2,12 @@
 
 class Router {
 
+  ROOT = '/mer-a/';
+  API_URL = 'api/';
+	API_REGIONS_ID = 'all/regions';
+	API_TYPES_ID = 'all/types';
+	API_LEGENDE = 'legende/';
+
   constructor(fileAriane, loader, soundManager) {
     this.fileAriane = fileAriane;
     this.loader = loader;
@@ -30,9 +36,9 @@ class Router {
     this.stop = false;
     this.soundStop = true;
 
-    $(this.scene1.elmt).load(`/mer-a/html/fond/parallax1.html`, () => {
+    $(this.scene1.elmt).load(`${this.ROOT}html/fond/parallax1.html`, () => {
       this.loadParralax(1);
-      $(this.scene2.elmt).load(`/mer-a/html/fond/parallax2.html`, () => {
+      $(this.scene2.elmt).load(`${this.ROOT}html/fond/parallax2.html`, () => {
         this.initAnim();
       });
     });
@@ -47,7 +53,7 @@ class Router {
       if(path === 'accueil' || fond !== undefined && this.fondActuel !== fond) loader.hide();
       this.deleteCharacter();
       this.changeFond(idFond);
-      $('#content').load(`/mer-a/html/${path}.html`).fadeIn('slow', () => {
+      $('#content').load(`${this.ROOT}html/${path}.html`).fadeIn('slow', () => {
         if(path === 'departements') {
           this.soundManager.startSound();
         }
