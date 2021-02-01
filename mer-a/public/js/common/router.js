93998b4cd105220fd1554ea62c412fdad6ebd752
Your Name <you@example.com>
2021-02-01 18:31:03 +0100

    Ajout particules + Correction filtre accueil

--- mer-a/public/js/common/router.js
+++ mer-a/public/js/common/router.js
@@ -48,9 +48,13 @@ class Router {
     const fond = this.fonds.find(fond => fond.id === idFond);
     if(fond !== undefined && this.fondActuel !== fond) this.loader.show();
     this.stopAnim();
+    if(window["pJSDom"] instanceof Array) for(let pJSD of window["pJSDom"]) 
+      pJSD.pJS.fn.vendors.destroypJS();
+    window["pJSDom"] = [];
 
     this.data = data;
     $('#content').fadeOut('slow', () => {
+      if(path === 'accueil') document.querySelector('#scene1').style.filter = 'brightness(50%)';
       if(path === 'accueil' || fond !== undefined && this.fondActuel !== fond) loader.hide();
       this.deleteCharacter();
       this.changeFond(idFond);
