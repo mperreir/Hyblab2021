05fe4d4c6c6f019c5cc4afbe4af5b66fc2caba52
Andréa Gugole <andrea.gugole@etu.univ-nantes.fr>
2021-02-01 18:35:55 +0100

    ajout gifs

--- proximite-a/src/js/ThemeSelector.jsx
+++ proximite-a/src/js/ThemeSelector.jsx
@@ -7,7 +7,11 @@ import curieux_img from '../img/Perso-hyblab-05.png'
 import famille_img from '../img/Perso-hyblab-06.png'
 import '../css/theme.css'
 import equivalent from './equivalent.js'
-
+import fetard_gif from '../img/Perso-animation fêtard.gif'
+import sportif_gif from '../img/Perso-animation sportif.gif'
+import gourmet_gif from '../img/Perso-animation 2.gif'
+import curieux_gif from '../img/Perso-animation culturel.gif'
+import famille_gif from '../img/Perso-animation 2_2.gif'
 class ThemeSelector extends React.Component {
     render() {
         const { onNextPage, onPreviousPage } = this.props;
@@ -19,35 +23,35 @@ class ThemeSelector extends React.Component {
                         <div class="col">
                             <input type="radio" name="theme" id="fetard" class="input-hidden" onClick={() => { this.props.updateSelect(1)}}/>
                             <label htmlFor="fetard">
-                                <img src={fetard_img} alt="fetard" class="img_bas"/>
+                                <img src={fetard_img} alt="fetard" class="img_bas" onMouseEnter={e => e.target.src = fetard_gif} onMouseLeave={e => e.target.src = fetard_img}/>
                             </label>
                         </div>
 
                         <div class="col">
                             <input type="radio" name="theme" id="sportif" class="input-hidden" onClick={() => { this.props.updateSelect(2) }}/>
                             <label htmlFor="sportif">
-                                <img src={sportif_img} alt="sportif" class="img_hte"/>
+                                <img src={sportif_img} alt="sportif" class="img_hte" onMouseEnter={e => e.target.src = sportif_gif} onMouseLeave={e => e.target.src = sportif_gif}/>
                             </label>
                         </div>
 
                         <div class="col">
-                            <input type="radio" name="theme" id="gourmet" class="input-hidden" onClick={() => { this.props.updateSelect(3) }}/>
+                            <input type="radio" name="theme" id="gourmet" class="input-hidden"  onClick={() => { this.props.updateSelect(3) }}/>
                             <label htmlFor="gourmet">
-                                <img src={gourmet_img} alt="gourmet" class="img_bas"/>
+                                <img src={gourmet_img} alt="gourmet" class="img_bas" onMouseEnter={e => e.target.src = gourmet_gif} onMouseLeave={e => e.target.src = gourmet_img}/>
                             </label>
                         </div>
 
                         <div class="col">
                             <input type="radio" name="theme" id="curieux" class="input-hidden" onClick={() => { this.props.updateSelect(4) }}/>
                             <label htmlFor="curieux">
-                                <img src={curieux_img} alt="curieux" class="img_hte"/>
+                                <img src={curieux_img} alt="curieux" class="img_hte" onMouseEnter={e => e.target.src = curieux_gif} onMouseLeave={e => e.target.src = curieux_img}/>
                             </label>
                         </div>
 
                         <div class="col">
                             <input type="radio" name="theme" id="famille" class="input-hidden" onClick={() => { this.props.updateSelect(5) }}/>
                             <label htmlFor="famille">
-                                <img src={famille_img} alt="famille" class="img_bas"/>
+                                <img src={famille_img} alt="famille" class="img_bas" onMouseEnter={e => e.target.src = famille_gif} onMouseLeave={e => e.target.src = famille_img}/>
                             </label>
                         </div>
                     </div>
