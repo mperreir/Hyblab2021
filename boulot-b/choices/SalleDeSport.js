d6c8fcefabfa637aa81cc27281e55e48bc1d8936
E17E609E <evan.gisdal@etu.univ-nantes.fr>
2021-02-03 17:50:46 +0100

    Correction description des salles de sport

--- boulot-b/choices/SalleDeSport.js
+++ boulot-b/choices/SalleDeSport.js
@@ -2,7 +2,7 @@ const Choice = require("./Choice");
 
 class SalleDeSport extends Choice {
     theme = "fitness-health-club"
-    description = "La salle de sport Basic Fit est sur ton chemin ! Une belle occasion de te défouler après ta journée."
+    description = "Nous avons trouvé cette salle de sport sur ton chemin ! Une belle occasion de te défouler après ta journée."
     name = "SalleSport"
 }
 
